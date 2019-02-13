ad0a06dee1fc4cd331bc26e1fdd5a0020ae2fef4
sibirrer <sibirrer@gmail.com>
2019-02-12 16:32:54 -0800

    test arrival time surface

--- test/test_Plots/test_output_plots.py
+++ test/test_Plots/test_output_plots.py
@@ -198,7 +198,7 @@ class TestOutputPlots(object):
         f, ax = plt.subplots(1, 1, figsize=(4, 4))
         lensModel = LensModel(lens_model_list=['SIS'])
         kwargs_lens = [{'theta_E': 1., 'center_x': 0, 'center_y': 0}]
-        output_plots.lens_model_plot(ax, lensModel, kwargs_lens, numPix=10, deltaPix=0.5, sourcePos_x=0, sourcePos_y=0,
+        output_plots.arrival_time_surface(ax, lensModel, kwargs_lens, numPix=10, deltaPix=0.5, sourcePos_x=0, sourcePos_y=0,
                                      point_source=True, with_caustics=True)
         plt.close()
 

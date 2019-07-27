8876b15a5e19d53127ce92d4c7400969b5357a5f
sibirrer <sibirrer@gmail.com>
2019-07-26 22:13:04 -0700

    meged with simulation_development, incl travis reconfiguratoin

---
+++ lenstronomy/LightModel/Profiles/merged_profiles.py
@@ -0,0 +1,9 @@
+class MergedProfiles(object):
+    """
+    this class describes a superposition of different profiles meant to be used jointly with one single linear
+    parameter. This allows to link it to a mass model too with the same properties.
+    The individual 'amp' parameters get replaced by a ratio relative to the first profile
+    """
+
+    def __init__(self):
+        pass

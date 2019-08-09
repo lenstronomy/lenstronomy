63bfc70d80401afb41fd85c64c3a90da7113adac
sibirrer <sibirrer@gmail.com>
2019-08-09 13:31:13 -0700

    documentation of general light profile added

--- lenstronomy/LightModel/light_model.py
+++ lenstronomy/LightModel/light_model.py
@@ -1,13 +1,22 @@
 __author__ = 'sibirrer'
 
-#this file contains a class which describes the surface brightness of the light models
 
 from lenstronomy.LightModel.linear_basis import LinearBasis
 
 
 class LightModel(LinearBasis):
     """
-    class to handle source and lens light models
+    class to handle extended surface brightness profiels (for e.g. source and lens light)
+
+    all profiles come with a surface_brightness parameterization (in units per square angle and independent of
+    the pixel scale).
+    The parameter 'amp' is the linear scaling parameter of surface brightness.
+    Some functional forms come with a total_flux() definition that provide the integral of the surface brightness for a
+    given set of parameters.
+
+    The SimulationAPI module allows to use astronomical magnitudes to be used and translated into the surface brightness
+    conventions of this module given a magnitude zero point.
+
     """
 
     def __init__(self, light_model_list, deflection_scaling_list=None, source_redshift_list=None,

cf04de7556dd9eb167d27cf04331cfbfc82fee74
Anowar Jaman Shajib <ajshajib@gmail.com>
2019-08-11 21:13:47 -0700

    Add resuming option for dypolychord with MPI

--- test/test_Workflow/test_fitting_sequence.py
+++ test/test_Workflow/test_fitting_sequence.py
@@ -221,8 +221,14 @@ class TestFittingSequence(object):
         kwargs_dypolychord = {
             'sampler_type': 'DYPOLYCHORD',
             'kwargs_run': {
-                'ninit': 8,
-                'nlive_const': 10,
+                'ninit': 20,
+                'nlive_const': 30,
+                #'seed_increment': 1,
+                'resume_dyn_run': False,
+                #'init_step': 10,
+            },
+            'polychord_settings': {
+                'seed': 1
             },
             'dypolychord_dynamic_goal': 0.8, # 1 for posterior-only, 0 for evidence-only
             'remove_output_dir': True,

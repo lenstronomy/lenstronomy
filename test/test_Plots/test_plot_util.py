cb669f0498226a018b9c35685928fc5b50ef889c
sibirrer <sibirrer@gmail.com>
2019-11-07 12:19:44 -0800

    test of plot_util improved and point_source plotting with zipping in place

--- test/test_Plots/test_plot_util.py
+++ test/test_Plots/test_plot_util.py
@@ -25,6 +25,25 @@ class TestPlotUtil(object):
         plot_util.text_description(ax, d=3, text='test', color='w', backgroundcolor='k', flipped=True)
         plt.close()
 
+    def test_source_position_plot(self):
+        from lenstronomy.PointSource.point_source import PointSource
+        from lenstronomy.LensModel.lens_model import LensModel
+        lensModel = LensModel(lens_model_list=['SIS'])
+        ps = PointSource(point_source_type_list=['UNLENSED', 'LENSED_POSITION', 'SOURCE_POSITION'], lensModel=lensModel)
+        kwargs_lens = [{'theta_E': 1., 'center_x': 0, 'center_y': 0}]
+        kwargs_ps = [{'ra_image': [1., 1.], 'dec_image': [0, 1], 'point_amp': [1, 1]},
+                          {'ra_image': [1.], 'dec_image': [1.], 'point_amp': [10]},
+                          {'ra_source': 0.1, 'dec_source': 0, 'point_amp': 1.}]
+        ra_source, dec_source = ps.source_position(kwargs_ps, kwargs_lens)
+        from lenstronomy.Data.coord_transforms import Coordinates
+        coords_source = Coordinates(transform_pix2angle=np.array([[1, 0], [0, 1]])* 0.1,
+                                    ra_at_xy_0=-2,
+                                    dec_at_xy_0=-2)
+
+        f, ax = plt.subplots(1, 1, figsize=(4, 4))
+        plot_util.source_position_plot(ax, coords_source, ra_source, dec_source)
+        plt.close()
+
 
 if __name__ == '__main__':
     pytest.main()

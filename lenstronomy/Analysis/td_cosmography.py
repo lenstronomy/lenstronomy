73d86d6f918a43a87a8c7629fd758f419c33bad4
sibirrer <sibirrer@gmail.com>
2019-11-11 22:07:50 -0800

    TDCosmography class finished

--- lenstronomy/Analysis/td_cosmography.py
+++ lenstronomy/Analysis/td_cosmography.py
@@ -10,7 +10,7 @@ from lenstronomy.Cosmo.lens_cosmo import LensCosmo
 from lenstronomy.Analysis.kinematics_api import KinematicAPI
 
 
-class TDCosmography(object):
+class TDCosmography(KinematicAPI):
     """
     class equipped to perform a cosmographic analysis from a lens model with added measurements of time delays and
     kinematics.
@@ -25,12 +25,14 @@ class TDCosmography(object):
 
         if cosmo_fiducial is None:
             cosmo_fiducial = default_cosmology.get()
+        self._z_lens = z_lens
+        self._z_source = z_source
         self._cosmo_fiducial = cosmo_fiducial
         self._lens_cosmo = LensCosmo(z_lens=z_lens, z_source=z_source, cosmo=self._cosmo_fiducial)
-        self._kinematic_api = KinematicAPI(z_lens, z_source, kwargs_model, cosmo=self._cosmo_fiducial,
-                                           lens_model_kinematics_bool=lens_model_kinematics_bool,
-                                           light_model_kinematics_bool=light_model_kinematics_bool)
         self.LensModel, self.SourceModel, self.LensLightModel, self.PointSource, extinction_class = class_creator.create_class_instances(all_models=True, **kwargs_model)
+        super(TDCosmography, self).__init__(z_lens=z_lens, z_source=z_source, kwargs_model=kwargs_model,
+                                            cosmo=cosmo_fiducial, lens_model_kinematics_bool=lens_model_kinematics_bool,
+                                            light_model_kinematics_bool=light_model_kinematics_bool)
 
     def time_delays(self, kwargs_lens, kwargs_ps, kappa_ext=0):
         """
@@ -80,15 +82,15 @@ class TDCosmography(object):
          if set to None will be computed in this function with default settings that may not be accurate.
         :return: dimensionless velocity dispersion (see e.g. Birrer et al. 2016, 2019)
         """
-        sigma_v = self._kinematic_api.model_velocity_dispersion(kwargs_lens=kwargs_lens,
+        sigma_v = self.model_velocity_dispersion(kwargs_lens=kwargs_lens,
                                                                 kwargs_lens_light=kwargs_lens_light,
                                                                 kwargs_anisotropy=kwargs_anisotropy,
                                                                 r_eff=r_eff, theta_E=theta_E, gamma=gamma)
+        sigma_v *= 1000 # convert from [km/s] to  [m/s]
         J = sigma_v ** 2 * self._lens_cosmo.D_ds / self._lens_cosmo.D_s / const.c ** 2
         return J
 
-    @staticmethod
-    def D_dt_from_time_delay(d_fermat_model, dt_measured, kappa_s=0, kappa_ds=0, kappa_d=0):
+    def Ddt_from_time_delay(self, d_fermat_model, dt_measured, kappa_s=0, kappa_ds=0, kappa_d=0):
         """
         Time-delay distance in units of Mpc from the modeled Fermat potential and measured time delay from an image pair.
 
@@ -100,8 +102,7 @@ class TDCosmography(object):
         D_dt = D_dt_model * (1-kappa_ds) / (1 - kappa_s) / (1 - kappa_d)
         return D_dt
 
-    @staticmethod
-    def Ds_Dds_from_kinematics(sigma_v, J, kappa_s=0, kappa_ds=0):
+    def Ds_Dds_from_kinematics(self, sigma_v, J, kappa_s=0, kappa_ds=0):
         """
         computes the estimate of the ratio of angular diameter distances Ds/Dds from the kinematic estimate of the lens
         and the measured dispersion.
@@ -110,6 +111,25 @@ class TDCosmography(object):
         :param J: dimensionless kinematic constraint (see Birrer et al. 2016, 2019)
         :return: Ds/Dds
         """
-        Ds_Dds_model = (sigma_v / 1000) ** 2 / const.c ** 2 / J
+        Ds_Dds_model = (sigma_v * 1000) ** 2 / const.c ** 2 / J
         Ds_Dds = Ds_Dds_model * (1 - kappa_ds) / (1 - kappa_s)
         return Ds_Dds
+
+    def Ddt_Dd_from_time_delay_and_kinematics(self, d_fermat_model, dt_measured, sigma_v_measured, J, kappa_s=0,
+                                              kappa_ds=0, kappa_d=0):
+        """
+
+        :param d_fermat_model:
+        :param dt_measured:
+        :param sigma_v_measured:
+        :param J:
+        :param kappa_s:
+        :param kappa_ds:
+        :param kappa_d:
+        :return:
+        """
+        D_dt = self.Ddt_from_time_delay(d_fermat_model, dt_measured, kappa_s=kappa_s, kappa_ds=kappa_ds,
+                                        kappa_d=kappa_d)
+        Ds_Dds = self.Ds_Dds_from_kinematics(sigma_v_measured, J, kappa_s=kappa_s, kappa_ds=kappa_ds)
+        Dd = D_dt / Ds_Dds / (1 + self._z_lens)
+        return D_dt, Dd
--- test/test_Analysis/test_td_cosmography.py
+++ test/test_Analysis/test_td_cosmography.py
@@ -7,6 +7,7 @@ import numpy as np
 from lenstronomy.Analysis.td_cosmography import TDCosmography
 from lenstronomy.LensModel.lens_model import LensModel
 from lenstronomy.LensModel.Solver.lens_equation_solver import LensEquationSolver
+from lenstronomy.Util import constants as const
 
 
 class TestTDCosmography(object):
@@ -47,31 +48,60 @@ class TestTDCosmography(object):
         ratio = fermat_pot / fermat_pot_true
         assert np.max(np.abs(ratio)) > 1.05
 
-"""
+    def test_cosmo_inference(self):
 
-    def test_time_delays(self):
-        z_lens = 0.5
-        z_source = 1.5
-        kwargs_options = {'lens_model_list': ['SPEP'], 'point_source_model_list': ['LENSED_POSITION']}
-        e1, e2 = param_util.phi_q2_ellipticity(0, 0.7)
-        kwargs_lens = [{'theta_E': 1, 'gamma': 2, 'e1': e1, 'e2': e2}]
-        kwargs_else = [{'ra_image': [-1, 0, 1], 'dec_image': [0, 0, 0]}]
-        from astropy.cosmology import FlatLambdaCDM
-        cosmo = FlatLambdaCDM(H0=70, Om0=0.3, Ob0=0.05)
-        lensProp = KinematicAPI(z_lens, z_source, kwargs_options, cosmo=cosmo)
-        delays = lensProp.time_delays(kwargs_lens, kwargs_ps=kwargs_else, kappa_ext=0)
-        npt.assert_almost_equal(delays[0], -31.387590264501007, decimal=8)
-        npt.assert_almost_equal(delays[1], 0, decimal=8)
-        npt.assert_almost_equal(delays[2], -31.387590264501007, decimal=8)
-        kappa_ext = 0.1
-        delays_kappa = lensProp.time_delays(kwargs_lens, kwargs_ps=kwargs_else, kappa_ext=kappa_ext)
-        npt.assert_almost_equal(delays_kappa/(1.-kappa_ext), delays, decimal=8)
+        # set up a cosmology
+        # compute image postions
+        # compute J and velocity dispersion
+        D_dt = self.td_cosmo._lens_cosmo.D_dt
+        D_d = self.td_cosmo._lens_cosmo.D_d
+        D_s = self.td_cosmo._lens_cosmo.D_s
+        D_ds = self.td_cosmo._lens_cosmo.D_ds
+        fermat_potential_list = self.td_cosmo.fermat_potential(self.kwargs_lens, self.kwargs_ps)
+        dt_list = self.td_cosmo.time_delays(self.kwargs_lens, self.kwargs_ps, kappa_ext=0)
+        dt = dt_list[0] - dt_list[1]
+        d_fermat = fermat_potential_list[0] - fermat_potential_list[1]
 
-        kappa_ext = 0.1
-        delays_kappa = lensProp.time_delays(kwargs_lens, kwargs_ps=kwargs_else, kappa_ext=kappa_ext)
-        npt.assert_almost_equal(delays_kappa / (1. - kappa_ext), delays, decimal=8)
+        D_dt_infered = self.td_cosmo.Ddt_from_time_delay(d_fermat_model=d_fermat, dt_measured=dt)
+        npt.assert_almost_equal(D_dt_infered, D_dt, decimal=5)
+        r_eff = 0.5
+        kwargs_lens_light = [{'Rs': r_eff * 0.551, 'center_x': 0, 'center_y': 0}]
+        kwargs_anisotropy = {'r_ani': 1}
 
-"""
+        R_slit = 3.8
+        dR_slit = 1.
+        aperture_type = 'slit'
+        kwargs_aperture = {'aperture_type': aperture_type, 'center_ra': 0, 'width': dR_slit, 'length': R_slit,
+                           'angle': 0, 'center_dec': 0}
+        psf_fwhm = 0.7
+        kwargs_seeing = {'psf_type': 'GAUSSIAN', 'fwhm': psf_fwhm}
+        self.td_cosmo.kinematic_observation_settings(kwargs_aperture, kwargs_seeing)
+
+        anisotropy_model = 'OsipkovMerritt'
+        kwargs_numerics_galkin = {'sampling_number': 1000, 'interpol_grid_num': 500, 'log_integration': True,
+                                  'max_integrate': 10, 'min_integrate': 0.001}
+        self.td_cosmo.kinematics_modeling_settings(anisotropy_model, kwargs_numerics_galkin, analytic_kinematics=True,
+                                             Hernquist_approx=False, MGE_light=False, MGE_mass=False)
+
+        J = self.td_cosmo.velocity_dispersion_dimension_less(self.kwargs_lens, kwargs_lens_light, kwargs_anisotropy, r_eff=r_eff,
+                                           theta_E=self.kwargs_lens[0]['theta_E'], gamma=2)
+        sigma_v2 = J * D_s/D_ds * const.c ** 2
+        sigma_v = np.sqrt(sigma_v2) / 1000.  # convert to [km/s]
+        print(sigma_v, 'test sigma_v')
+        Ds_Dds = self.td_cosmo.Ds_Dds_from_kinematics(sigma_v, J, kappa_s=0, kappa_ds=0)
+        npt.assert_almost_equal(Ds_Dds, D_s/D_ds)
+
+        # now we perform a mass-sheet transform in the observables but leave the models identical with a convergence correction
+        kappa_s = 0.5
+        dt_list = self.td_cosmo.time_delays(self.kwargs_lens, self.kwargs_ps, kappa_ext=kappa_s)
+        sigma_v_kappa = sigma_v * np.sqrt(1-kappa_s)
+        dt = dt_list[0] - dt_list[1]
+        D_dt_infered, D_d_infered = self.td_cosmo.Ddt_Dd_from_time_delay_and_kinematics(d_fermat_model=d_fermat, dt_measured=dt,
+                                                                        sigma_v_measured=sigma_v_kappa, J=J, kappa_s=kappa_s,
+                                                                        kappa_ds=0, kappa_d=0)
+        npt.assert_almost_equal(D_dt_infered, D_dt, decimal=6)
+        npt.assert_almost_equal(D_d_infered, D_d, decimal=6)
+
 
 if __name__ == '__main__':
     pytest.main()

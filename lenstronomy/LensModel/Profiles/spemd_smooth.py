2daafc42503ce9daec262365d29fb755708d0af0
sibirrer <sibirrer@gmail.com>
2020-02-12 16:14:41 -0800

    updated documentation for lens equation solver, changed format for limits for fastell4py variables

--- lenstronomy/LensModel/Profiles/spemd_smooth.py
+++ lenstronomy/LensModel/Profiles/spemd_smooth.py
@@ -29,6 +29,9 @@ class SPEMD_SMOOTH(LensProfileBase):
     upper_limit_default = {'theta_E': 100, 'gamma': 100, 'e1': 0.5, 'e2': 0.5, 's_scale': 100, 'center_x': 100, 'center_y': 100}
 
     def __init__(self):
+        """
+
+        """
         try:
             from fastell4py import fastell4py
             self._fastell4py_bool = True
@@ -52,17 +55,10 @@ class SPEMD_SMOOTH(LensProfileBase):
         :param center_y: y-position of lens center
         :return: lensing potential
         """
-        phi_G, q = param_util.ellipticity2phi_q(e1, e2)
-        theta_E, gamma, q, phi_G, s_scale = self._parameter_constraints(theta_E, gamma, q, phi_G, s_scale)
-        x_shift = x - center_x
-        y_shift = y - center_y
-        q_fastell, gam, s2 = self.convert_params(theta_E, gamma, q, s_scale)
-
-        cos_phi = np.cos(phi_G)
-        sin_phi = np.sin(phi_G)
-        x1 = cos_phi*x_shift+sin_phi*y_shift
-        x2 = -sin_phi*x_shift+cos_phi*y_shift
-        if self._fastell4py_bool and self.is_not_empty(x1, x2):
+        x1, x2, q_fastell, gam, s2, q, phi_G = self.param_transform(x, y, theta_E, gamma, e1, e2, s_scale, center_x,
+                                                                    center_y)
+        compute_bool = self._parameter_constraints(q_fastell, gam, s2, q)
+        if self._fastell4py_bool and self.is_not_empty(x1, x2) and compute_bool:
             potential = self.fastell4py.ellipphi(x1, x2, q_fastell, gam, arat=q, s2=s2)
             n = len(np.atleast_1d(x))
             if n <= 1:
@@ -87,31 +83,19 @@ class SPEMD_SMOOTH(LensProfileBase):
         :param center_y: y-position of lens center
         :return: deflection angles alpha_x, alpha_y
         """
-        phi_G, q = param_util.ellipticity2phi_q(e1, e2)
-        x = np.array(x)
-        y = np.array(y)
-        theta_E, gamma, q, phi_G, s_scale = self._parameter_constraints(theta_E, gamma, q, phi_G, s_scale)
-        x_shift = x - center_x
-        y_shift = y - center_y
-        q_fastell, gam, s2 = self.convert_params(theta_E, gamma, q, s_scale)
-
-        cos_phi = np.cos(phi_G)
-        sin_phi = np.sin(phi_G)
-
-        x1 = cos_phi*x_shift+sin_phi*y_shift
-        x2 = -sin_phi*x_shift+cos_phi*y_shift
-
-        if self._fastell4py_bool and self.is_not_empty(x1, x2):
+        x1, x2, q_fastell, gam, s2, q, phi_G = self.param_transform(x, y, theta_E, gamma, e1, e2, s_scale, center_x,
+                                                                    center_y)
+        compute_bool = self._parameter_constraints(q_fastell, gam, s2, q)
+        if self._fastell4py_bool and self.is_not_empty(x1, x2) and compute_bool:
             f_x_prim, f_y_prim = self.fastell4py.fastelldefl(x1, x2, q_fastell, gam, arat=q, s2=s2)
         else:
             f_x_prim, f_y_prim =  np.zeros_like(x1), np.zeros_like(x1)
             Warning("SPEMD model output replaced by zeros as fastell4py package is not installed!")
+        cos_phi = np.cos(phi_G)
+        sin_phi = np.sin(phi_G)
+
         f_x = cos_phi*f_x_prim - sin_phi*f_y_prim
         f_y = sin_phi*f_x_prim + cos_phi*f_y_prim
-        n = len(np.atleast_1d(x))
-        #if n <= 1:
-        #    if np.shape(x) == ():
-        #        f_x, f_y = np.array(f_x[0]), np.array(f_y[0])
         return f_x, f_y
 
     def hessian(self, x, y, theta_E, gamma, e1, e2, s_scale, center_x=0, center_y=0):
@@ -128,20 +112,9 @@ class SPEMD_SMOOTH(LensProfileBase):
         :param center_y: y-position of lens center
         :return: Hessian components f_xx, f_yy, f_xy
         """
-        phi_G, q = param_util.ellipticity2phi_q(e1, e2)
-        theta_E, gamma, q, phi_G, s_scale = self._parameter_constraints(theta_E, gamma, q, phi_G, s_scale)
-        x = np.array(x)
-        y = np.array(y)
-        x_shift = x - center_x
-        y_shift = y - center_y
-        q_fastell, gam, s2 = self.convert_params(theta_E, gamma, q, s_scale)
-
-        cos_phi = np.cos(phi_G)
-        sin_phi = np.sin(phi_G)
-
-        x1 = cos_phi*x_shift+sin_phi*y_shift
-        x2 = -sin_phi*x_shift+cos_phi*y_shift
-        if self._fastell4py_bool and self.is_not_empty(x1, x2):
+        x1, x2, q_fastell, gam, s2, q, phi_G = self.param_transform(x, y, theta_E, gamma, e1, e2, s_scale, center_x, center_y)
+        compute_bool = self._parameter_constraints(q_fastell, gam, s2, q)
+        if self._fastell4py_bool and self.is_not_empty(x1, x2) and compute_bool:
             f_x_prim, f_y_prim, f_xx_prim, f_yy_prim, f_xy_prim = self.fastell4py.fastellmag(x1, x2, q_fastell, gam,
                                                                                              arat=q, s2=s2)
             n = len(np.atleast_1d(x))
@@ -164,6 +137,34 @@ class SPEMD_SMOOTH(LensProfileBase):
         f_xy = gamma2
         return f_xx, f_yy, f_xy
 
+    def param_transform(self, x, y, theta_E, gamma, e1, e2, s_scale, center_x=0, center_y=0):
+        """
+        transforms parameters in the format of fastell4py
+
+        :param x: x-coordinate (angle)
+        :param y: y-coordinate (angle)
+        :param theta_E: Einstein radius (angle), pay attention to specific definition!
+        :param gamma: logarithmic slope of the power-law profile. gamma=2 corresponds to isothermal
+        :param e1: eccentricity component
+        :param e2: eccentricity component
+        :param s_scale: smoothing scale in the center of the profile
+        :param center_x: x-position of lens center
+        :param center_y: y-position of lens center
+        :return: x_, y_, q_fastell, gam, s2, q, phi_G
+        """
+        phi_G, q = param_util.ellipticity2phi_q(e1, e2)
+        x = np.array(x)
+        y = np.array(y)
+        x_shift = x - center_x
+        y_shift = y - center_y
+        q_fastell, gam, s2 = self.convert_params(theta_E, gamma, q, s_scale)
+        cos_phi = np.cos(phi_G)
+        sin_phi = np.sin(phi_G)
+
+        x1 = cos_phi * x_shift + sin_phi * y_shift
+        x2 = -sin_phi * x_shift + cos_phi * y_shift
+        return x1, x2, q_fastell, gam, s2, q, phi_G
+
     @staticmethod
     def convert_params(theta_E, gamma, q, s_scale):
         """
@@ -172,6 +173,7 @@ class SPEMD_SMOOTH(LensProfileBase):
         :param theta_E: Einstein radius
         :param gamma: 3D power-law slope of mass profile
         :param q: axis ratio minor/major
+        :param s_scale: float, smoothing scale in the core
         :return: pre-factors to SPEMP profile for FASTELL
         """
         gam = (gamma-1)/2.
@@ -197,30 +199,24 @@ class SPEMD_SMOOTH(LensProfileBase):
             return True
 
     @staticmethod
-    def _parameter_constraints(theta_E, gamma, q, phi_G, s_scale):
+    def _parameter_constraints(q_fastell, gam, s2, q):
         """
         sets bounds to parameters due to numerical stability
-        :param theta_E: Einstein radius
-        :param gamma: 3d power-law slope
+
+        FASTELL has the following defintions:
+        The parameters are position (x1,x2), overall factor
+        (q), power (gam), axis ratio (arat) which is <=1, core radius
+        squared (s2), and the output potential (phi).
+        The projected mass density distribution, in units of the
+        critical density, is kappa(x1,x2)=q [u2+s2]^(-gam), where
+        u2=[x1^2+x2^2/(arat^2)].
+
+        :param q_fastell: float, normalization of lens model, q_fastell = (3-gamma)/2. * (theta_E ** 2 / q) ** gam
+        :param gam: float, slope parameter, gam = (gamma-1)/2.
         :param q: axis ratio
-        :param phi_G: orientation angle
-        :param s_scale: smoothing scale of the core
-        :return: input parameters within numerical bounds
+        :param s2: square of smoothing scale of the core
+        :return: bool of whether or not to let the fastell provide to be evaluated or instead return zero(s)
         """
-        if theta_E < 0:
-            theta_E = 0
-        if s_scale < 0.00000001:
-            s_scale = 0.00000001
-        if gamma < 1.2:
-            gamma = 1.2
-            theta_E = 0
-        if gamma > 2.7:
-            gamma = 2.7
-            theta_E = 0
-        if q < 0.02:
-            q = 0.02
-            theta_E = 0
-        if q > 1:
-            q = 1.
-            theta_E = 0
-        return theta_E, gamma, q, phi_G, s_scale
+        if q_fastell < 0 or s2 < 0.0000000000001 or q > 1 or q < 0.01 or gam > 0.999 or gam < 0.001:
+            return False
+        return True

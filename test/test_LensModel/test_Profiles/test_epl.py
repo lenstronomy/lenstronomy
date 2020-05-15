1751b52f996de10ce8fcd751570539dfa6cb8139
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-05-15 19:35:49 +0100

    improve tests

--- test/test_LensModel/test_Profiles/test_epl.py
+++ test/test_LensModel/test_Profiles/test_epl.py
@@ -31,6 +31,26 @@ class TestEPL(object):
         delta_f_nie = values_nie[0] - values_nie[1]
         npt.assert_almost_equal(delta_f, delta_f_nie, decimal=5)
 
+        q = 0.8
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        x = np.array([1., 2])
+        y = np.array([2, 0])
+        values = self.EPL.function(x, y, phi_E, e1, e2, t)
+        values_nie = self.NIE.function(x, y, phi_E, e1, e2, 0.)
+        delta_f = values[0] - values[1]
+        delta_f_nie = values_nie[0] - values_nie[1]
+        npt.assert_almost_equal(delta_f, delta_f_nie, decimal=5)
+
+        q = 0.4
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        x = np.array([1., 2])
+        y = np.array([2, 0])
+        values = self.EPL.function(x, y, phi_E, e1, e2, t)
+        values_nie = self.NIE.function(x, y, phi_E, e1, e2, 0.)
+        delta_f = values[0] - values[1]
+        delta_f_nie = values_nie[0] - values_nie[1]
+        npt.assert_almost_equal(delta_f, delta_f_nie, decimal=5)
+
     def test_derivatives(self):
         x = np.array([1])
         y = np.array([2])
@@ -65,6 +85,20 @@ class TestEPL(object):
         npt.assert_almost_equal(f_xx, f_xx_nie, decimal=4)
         npt.assert_almost_equal(f_yy, f_yy_nie, decimal=4)
         npt.assert_almost_equal(f_xy, f_xy_nie, decimal=4)
+
+    def test_static(self):
+        x, y = 1., 1.
+        phi_G, q = 0.3, 0.8
+        e1, e2 = param_util.phi_q2_ellipticity(phi_G, q)
+        kwargs_lens = {'theta_E': 1., 't': 1.5, 'e1': e1, 'e2': e2}
+        f_ = self.EPL.function(x, y, **kwargs_lens)
+        self.EPL.set_static(**kwargs_lens)
+        f_static = self.EPL.function(x, y, **kwargs_lens)
+        npt.assert_almost_equal(f_, f_static, decimal=8)
+        self.EPL.set_dynamic()
+        kwargs_lens = {'theta_E': 2., 't': 0.5, 'e1': e1, 'e2': e2}
+        f_dyn = self.EPL.function(x, y, **kwargs_lens)
+        assert f_dyn != f_static
 
 
 if __name__ == '__main__':

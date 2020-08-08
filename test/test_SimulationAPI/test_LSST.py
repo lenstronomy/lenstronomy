2db3815c548090a8c15b0a1d708bd8b0af25c9f5
Madi Ueland <mueland@stanford.edu>
2020-08-07 18:50:24 -1000

    Add u, z, y bands to LSST configs

--- test/test_SimulationAPI/test_LSST.py
+++ test/test_SimulationAPI/test_LSST.py
@@ -3,20 +3,31 @@ from lenstronomy.SimulationAPI.ObservationConfig.LSST import LSST
 from lenstronomy.SimulationAPI.observation_api import Instrument, SingleBand
 import lenstronomy.Util.util as util
 
+
 class TestLSST(unittest.TestCase):
 
     def setUp(self):
+        self.u = LSST(band='u')
         self.g = LSST()  # default is g_band
         self.r = LSST(band='r')
         self.i = LSST(band='i')
+        self.z = LSST(band='z')
+        self.y = LSST(band='Y')  # same as band='y'
 
+        kwargs_u_band = self.u.kwargs_single_band()
         kwargs_g_band = self.g.kwargs_single_band()
         kwargs_r_band = self.r.kwargs_single_band()
         kwargs_i_band = self.i.kwargs_single_band()
+        kwargs_z_band = self.z.kwargs_single_band()
+        kwargs_y_band = self.y.kwargs_single_band()
 
+
+        self.u_band = SingleBand(**kwargs_u_band)
         self.g_band = SingleBand(**kwargs_g_band)
         self.r_band = SingleBand(**kwargs_r_band)
         self.i_band = SingleBand(**kwargs_i_band)
+        self.z_band = SingleBand(**kwargs_z_band)
+        self.y_band = SingleBand(**kwargs_y_band)
 
         # dictionaries mapping LSST kwargs to SingleBand kwargs
         self.camera_settings = {'read_noise': '_read_noise',
@@ -38,7 +49,10 @@ class TestLSST(unittest.TestCase):
         self.assertEqual(explicit_g.obs, default.obs)
 
         with self.assertRaises(ValueError):
-            bad_band = LSST(band='u')
+            bad_band_1 = LSST(band='9')
+
+        with self.assertRaises(ValueError):
+            bad_band_2 = LSST(band='H')
 
         with self.assertRaises(ValueError):
             bad_psf = LSST(psf_type='blah')
@@ -54,9 +68,12 @@ class TestLSST(unittest.TestCase):
     def test_LSST_obs(self):
         # comparing obs settings in LSST instance with those in SingleBand instance
         for config, setting in self.obs_settings.items():
+            self.assertEqual(self.u.obs[config], getattr(self.u_band, setting), msg=f"{config} did not match")
             self.assertEqual(self.g.obs[config], getattr(self.g_band, setting), msg=f"{config} did not match")
             self.assertEqual(self.r.obs[config], getattr(self.r_band, setting), msg=f"{config} did not match")
             self.assertEqual(self.i.obs[config], getattr(self.i_band, setting), msg=f"{config} did not match")
+            self.assertEqual(self.z.obs[config], getattr(self.z_band, setting), msg=f"{config} did not match")
+            self.assertEqual(self.y.obs[config], getattr(self.y_band, setting), msg=f"{config} did not match")
 
     def test_kwargs_single_band(self):
         kwargs_g = util.merge_dicts(self.g.camera, self.g.obs)

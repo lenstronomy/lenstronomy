5087d352086393b27749a87d830037a30da1a3e4
sibirrer <sibirrer@gmail.com>
2020-08-20 09:53:13 -0700

    format string changed in Multipole lens model, minor modification to PR #201

--- lenstronomy/LensModel/Profiles/multipole.py
+++ lenstronomy/LensModel/Profiles/multipole.py
@@ -1,10 +1,10 @@
 __author__ = 'lynevdv'
 
-
 import numpy as np
 
 import lenstronomy.Util.param_util as param_util
 from lenstronomy.LensModel.Profiles.base_profile import LensProfileBase
+
 
 class Multipole(LensProfileBase):
     """
@@ -22,38 +22,48 @@ class Multipole(LensProfileBase):
         """
         Lensing potential of multipole contribution (for 1 component with m>=2)
         This uses the same definitions as Xu et al.(2013) in Appendix B3 https://arxiv.org/pdf/1307.4220.pdf
-        m : int, multipole order, m>=2
-        a_m : float, multipole strength
-        phi_m : float, multipole orientation in radian
+
+        :param m: int, multipole order, m>=2
+        :param a_m: float, multipole strength
+        :param phi_m: float, multipole orientation in radian
+        :param center_x: x-position
+        :param center_y: x-position
+        :return: lensing potential
         """
         r, phi = param_util.cart2polar(x, y, center_x=center_x, center_y=center_y)
-        f_ = r*a_m /(1-m**2) *np.cos(m*(phi-phi_m))
+        f_ = r*a_m /(1-m**2) * np.cos(m*(phi-phi_m))
         return f_
+
     def derivatives(self,x,y, m, a_m, phi_m, center_x=0, center_y=0):
         """
         Deflection of a multipole contribution (for 1 component with m>=2)
         This uses the same definitions as Xu et al.(2013) in Appendix B3 https://arxiv.org/pdf/1307.4220.pdf
-        m : int, multipole order, m>=2
-        a_m : float, multipole strength
-        phi_m : float, multipole orientation in radian
+
+        :param m: int, multipole order, m>=2
+        :param a_m: float, multipole strength
+        :param phi_m: float, multipole orientation in radian
+        :param center_x: x-position
+        :param center_y: x-position
+        :return: deflection angles alpha_x, alpha_y
         """
         r, phi = param_util.cart2polar(x, y, center_x=center_x, center_y=center_y)
-        f_x = np.cos(phi)*a_m/(1-m**2) *np.cos(m*(phi-phi_m)) + np.sin(phi)*m*a_m/(1-m**2)*np.sin(m*(phi-phi_m))
-        f_y = np.sin(phi)*a_m/(1-m**2) *np.cos(m*(phi-phi_m)) - np.cos(phi)*m*a_m/(1-m**2)*np.sin(m*(phi-phi_m))
-        return f_x,f_y
+        f_x = np.cos(phi)*a_m/(1-m**2) * np.cos(m*(phi-phi_m)) + np.sin(phi)*m*a_m/(1-m**2)*np.sin(m*(phi-phi_m))
+        f_y = np.sin(phi)*a_m/(1-m**2) * np.cos(m*(phi-phi_m)) - np.cos(phi)*m*a_m/(1-m**2)*np.sin(m*(phi-phi_m))
+        return f_x, f_y
+
     def hessian(self,x,y, m, a_m, phi_m, center_x=0, center_y=0):
         """
         Hessian of a multipole contribution (for 1 component with m>=2)
         This uses the same definitions as Xu et al.(2013) in Appendix B3 https://arxiv.org/pdf/1307.4220.pdf
-        m : int, multipole order, m>=2
-        a_m : float, multipole strength
-        phi_m : float, multipole orientation in radian
+
+        :param m: int, multipole order, m>=2
+        :param a_m: float, multipole strength
+        :param phi_m: float, multipole orientation in radian
+        :param center_x: x-position
+        :param center_y: x-position
+        :return: f_xx, f_yy, f_xy
         """
         r, phi = param_util.cart2polar(x, y, center_x=center_x, center_y=center_y)
-        # if isinstance(r, int) or isinstance(r, float):
-        #     r = max(0.000001, r)
-        # else:
-        #     r[r < 0.000001] = 0.000001
         r = np.maximum(r, 0.000001)
         f_xx = 1./r * np.sin(phi)**2 * a_m *np.cos(m*(phi-phi_m))
         f_yy = 1./r * np.cos(phi)**2 * a_m *np.cos(m*(phi-phi_m))

7467264de8ddfdd0c7e2064f09706c73377c167a
Aymeric Galan <aymeric.galan@gmail.com>
2020-09-02 15:21:20 +0200

    Minor improvements

--- lenstronomy/ImSim/image_model.py
+++ lenstronomy/ImSim/image_model.py
@@ -122,6 +122,18 @@ class ImageModel(object):
 
     def _source_surface_brightness_analytical(self, kwargs_source, kwargs_lens=None, kwargs_extinction=None, kwargs_special=None,
                                               unconvolved=False, de_lensed=False, k=None):
+        """
+
+        computes the source surface brightness distribution
+
+        :param kwargs_source: list of keyword arguments corresponding to the superposition of different source light profiles
+        :param kwargs_lens: list of keyword arguments corresponding to the superposition of different lens profiles
+        :param kwargs_extinction: list of keyword arguments of extinction model
+        :param unconvolved: if True: returns the unconvolved light distribution (prefect seeing)
+        :param de_lensed: if True: returns the un-lensed source surface brightness profile, otherwise the lensed.
+        :param k: integer, if set, will only return the model of the specific index
+        :return: 1d array of surface brightness pixels
+        """
         if len(self.SourceModel.profile_type_list) == 0:
             return np.zeros((self.Data.num_pixel_axes))
         ra_grid, dec_grid = self.ImageNumerics.coordinates_evaluate
@@ -137,7 +149,7 @@ class ImageModel(object):
     def _source_surface_brightness_pixelbased(self, kwargs_source, kwargs_lens=None, kwargs_extinction=None, kwargs_special=None,
                                               unconvolved=False, de_lensed=False, k=None, update_mapping=True):
         """
-        computes the source surface brightness distribution
+        computes the source surface brightness distribution, using pixel-based solver for light profiles (from SLITronomy)
 
         :param kwargs_source: list of keyword arguments corresponding to the superposition of different source light profiles
         :param kwargs_lens: list of keyword arguments corresponding to the superposition of different lens profiles
@@ -145,7 +157,7 @@ class ImageModel(object):
         :param unconvolved: if True: returns the unconvolved light distribution (prefect seeing)
         :param de_lensed: if True: returns the un-lensed source surface brightness profile, otherwise the lensed.
         :param k: integer, if set, will only return the model of the specific index
-        :param update_mapping: if False, prevent the pixelated lensing mapping to be updated (save computation time). 
+        :param update_mapping: if False, prevent the pixelated lensing mapping to be updated (saves computation time if previously computed). 
         :return: 1d array of surface brightness pixels
         """
         if len(self.SourceModel.profile_type_list) == 0:
@@ -179,7 +191,9 @@ class ImageModel(object):
         :return: 1d array of surface brightness pixels
         """
         if self.pixelbased_bool is True:
-            return self._lens_surface_brightness_pixelbased(kwargs_lens_light, unconvolved=unconvolved, k=k)
+            if unconvolved is True:
+                raise ValueError("Lens light pixel-based modelling does not perform deconvolution")
+            return self._lens_surface_brightness_pixelbased(kwargs_lens_light, k=k)
         else:
             return self._lens_surface_brightness_analytical(kwargs_lens_light, unconvolved=unconvolved, k=k)
 
@@ -197,19 +211,17 @@ class ImageModel(object):
         lens_light_final = self.ImageNumerics.re_size_convolve(lens_light, unconvolved=unconvolved)
         return lens_light_final
 
-    def _lens_surface_brightness_pixelbased(self, kwargs_lens_light, unconvolved=False, k=None):
+    def _lens_surface_brightness_pixelbased(self, kwargs_lens_light, k=None):
         """
 
-        computes the lens surface brightness distribution
+        computes the lens surface brightness distribution , using pixel-based solver for light profiles (from SLITronomy)
+        Important: SLITronomy does not solve for deconvolved lens light, hence the returned map is convolved with the PSF.
 
         :param kwargs_lens_light: list of keyword arguments corresponding to different lens light surface brightness profiles
-        :param unconvolved: if True, returns unconvolved surface brightness (perfect seeing), otherwise convolved with PSF kernel
         :return: 1d array of surface brightness pixels
         """
         ra_grid, dec_grid = self.ImageNumerics.coordinates_evaluate
         lens_light = self.LensLightModel.surface_brightness(ra_grid, dec_grid, kwargs_lens_light, k=k)
-        if unconvolved is True:
-            raise ValueError("Lens light pixel-based modelling does not perform deconvolution")
         lens_light_final = util.array2image(lens_light)
         return lens_light_final
 

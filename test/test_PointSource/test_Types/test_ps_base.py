ce9f1309ed96cfdc5f55928901ff5b7a55b57508
sibirrer <sibirrer@gmail.com>
2020-12-28 19:22:14 -0800

    test PSBase class

--- test/test_PointSource/test_Types/test_ps_base.py
+++ test/test_PointSource/test_Types/test_ps_base.py
@@ -1,4 +1,22 @@
 import pytest
+import unittest
+
+from lenstronomy.PointSource.Types.base_ps import PSBase
+from lenstronomy.LensModel.lens_model import LensModel
+
+
+class TestPSBase(object):
+
+    def setup(self):
+        self.base = PSBase(lens_model=LensModel(lens_model_list=[]), fixed_magnification=False, additional_image=False)
+
+    def test_update_lens_model(self):
+        self.base.update_lens_model(lens_model_class=None)
+        assert self.base._solver is None
+
+        base = PSBase()
+        base.update_lens_model(lens_model_class=LensModel(lens_model_list=['SIS']))
+        assert base._solver is not None
 
 
 class TestUtil(object):
@@ -6,7 +24,7 @@ class TestUtil(object):
     def setup(self):
         pass
 
-    def test_expand_t0_array(self):
+    def test_expand_to_array(self):
         from lenstronomy.PointSource.Types.base_ps import _expand_to_array
         array = 1
         num = 3
@@ -24,6 +42,20 @@ class TestUtil(object):
         array_out = _expand_to_array(array, num)
         assert len(array_out) == num
         assert array_out[1] == 1
+
+
+class TestRaise(unittest.TestCase):
+
+    def test_raise(self):
+        base = PSBase()
+        with self.assertRaises(ValueError):
+            base.image_position(kwargs_ps=None)
+        with self.assertRaises(ValueError):
+            base.source_position(kwargs_ps=None)
+        with self.assertRaises(ValueError):
+            base.image_amplitude(kwargs_ps=None)
+        with self.assertRaises(ValueError):
+            base.source_amplitude(kwargs_ps=None)
 
 
 if __name__ == '__main__':

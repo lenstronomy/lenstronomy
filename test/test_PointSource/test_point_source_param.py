46bb139e2d112e9d97281638a348ebc9b0436075
sibirrer <sibirrer@gmail.com>
2020-12-28 19:13:51 -0800

    point source parameter handling testing improved

--- test/test_PointSource/test_point_source_param.py
+++ test/test_PointSource/test_point_source_param.py
@@ -11,23 +11,19 @@ class TestParam(object):
     def setup(self):
         kwargs_fixed = [{}, {}, {}]
         num_point_sources_list = [4, 1, 1]
+        fixed_magnification_list = [True, False, False]
         point_source_model_list = ['LENSED_POSITION', 'SOURCE_POSITION', 'UNLENSED']
         self.param = PointSourceParam(model_list=point_source_model_list, kwargs_fixed=kwargs_fixed,
-                                      num_point_source_list=num_point_sources_list)
+                                      num_point_source_list=num_point_sources_list,
+                                      fixed_magnification_list=fixed_magnification_list)
         self.kwargs =[{'ra_image': np.array([0, 0, 0, 0]), 'dec_image': np.array([0, 0, 0, 0]),
-                       'point_amp': np.array([1, 1, 1, 1])},
+                       'source_amp': 1},
                       {'ra_source': 1, 'dec_source': 1, 'point_amp': np.array([1.])},
                       {'ra_image': [1], 'dec_image': [1], 'point_amp': np.array([1.])}]
-        #self.kwargs_sigma = [{'pos_sigma': 1, 'point_amp_sigma': 1}, {'pos_sigma': 1, 'point_amp_sigma': 1}, {'pos_sigma': 1, 'point_amp_sigma': 1}]
-
-        self.kwargs_mean = []
-        for i in range(len(self.kwargs)):
-            kwargs_mean_k = self.kwargs[i].copy()
-            #kwargs_mean_k.update(self.kwargs_sigma[i])
-            self.kwargs_mean.append(kwargs_mean_k)
 
         self.param_linear = PointSourceParam(model_list=point_source_model_list, kwargs_fixed=[{}, {}, {}],
-                                      num_point_source_list=num_point_sources_list, linear_solver=False)
+                                      num_point_source_list=num_point_sources_list, linear_solver=False,
+                                             fixed_magnification_list=fixed_magnification_list)
 
     def test_get_setParams(self):
         args = self.param.setParams(self.kwargs)
@@ -47,11 +43,11 @@ class TestParam(object):
         assert num == 12
 
         num, list = self.param_linear.num_param()
-        assert num == 18
+        assert num == 15
 
     def test_num_param_linear(self):
         num = self.param.num_param_linear()
-        assert num == 6
+        assert num == 3
 
         num = self.param_linear.num_param_linear()
         assert num == 0

b1b73acd4484a404d0e6357260c18f195e8351b1
sibirrer <sibirrer@gmail.com>
2021-03-02 14:06:45 -0800

    this is a first attempt to allow a user to define environmental configurations for numba in a yaml file. Current PR is given a default file and a place-holder to allow a different path (we still need to decide where this absolute path could be, suggestions welcome). This PR is intended to allow more flexibility and to complement #243

--- lenstronomy/Util/numba_util.py
+++ lenstronomy/Util/numba_util.py
@@ -1,4 +1,8 @@
 import numba
+import lenstronomy
+import os
+import yaml
+
 
 """
 From pyautolens:
@@ -9,9 +13,28 @@ If on super computer:
 @numba.jit(nopython=True, cache=False, parallel=True)
 """
 
-nopython = True
-cache = True
-parallel = False
+
+# TODO define a special path outside the lenstronomy package to save a user configuration yaml file
+user_config_file = ''
+module_path = os.path.dirname(lenstronomy.__file__)
+default_config_file = os.path.join(module_path, 'Conf', 'numba_conf_default.yaml')
+
+if os.path.exists(user_config_file ):
+    conf_file = user_config_file
+else:
+    conf_file = default_config_file
+
+with open(conf_file) as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    numba_list = yaml.load(file, Loader=yaml.FullLoader)
+    nopython = numba_list['nopython']
+    cache = numba_list['cache']
+    parallel = numba_list['parallel']
+
+#nopython = True
+#cache = True
+#parallel = False
 
 __all__ = ['jit']
 

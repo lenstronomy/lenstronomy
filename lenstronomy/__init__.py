10caba5ff9f23710f02340606e496ef6816349c7
sibirrer <sibirrer@gmail.com>
2022-04-18 09:02:17 -0700

    ready for minor release 1.10.3

--- lenstronomy/__init__.py
+++ lenstronomy/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Simon Birrer'
 __email__ = 'sibirrer@gmail.com'
-__version__ = '1.10.2'
+__version__ = '1.10.3'
 __credits__ = 'ETH Zurich, UCLA, Stanford'
 
 from .Util.package_util import short, laconic

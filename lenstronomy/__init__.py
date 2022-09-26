80564ee9471d7bfe18c0fa3d9a9d02bee84f0fa1
sibirrer <sibirrer@gmail.com>
2022-09-26 14:37:19 -0700

    ready for release 1.11.0

--- lenstronomy/__init__.py
+++ lenstronomy/__init__.py
@@ -1,5 +1,5 @@
-__author__ = 'Simon Birrer and contributors'
-__email__ = 'sibirrer@gmail.com'
+__author__ = 'lenstronomy developers'
+__email__ = 'lenstronomy-dev@googlegroups.com'
 __version__ = '1.10.4'
 __credits__ = 'ETH Zurich, UCLA, Stanford, Stony Brook'
 

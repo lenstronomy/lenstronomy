ccac992bb7c3035ab50d0a5b03461a09e493c3c9
mgomer <mgomer@uliege.be>
2023-07-31 19:33:51 +0200

    fixed kin_NN_call test reference

--- test/test_Sampling/test_Likelihoods/test_kinematic_NN_call.py
+++ test/test_Sampling/test_Likelihoods/test_kinematic_NN_call.py
@@ -1,4 +1,4 @@
-from lenstronomy.Sampling.Likelihoods.kinematic_NN_call import kinematicNN
+from lenstronomy.Sampling.Likelihoods.kinematic_NN_call import KinematicNN
 import numpy as np
 import numpy.testing as npt
 import copy
@@ -11,7 +11,7 @@ class TestKinNNCall(object):
         self.example_input=np.array([8.56306356e-01,  6.77923305e-01,  1.46690845e+00,  3.00871520e+00,
                                       1.30421229e+00, 8.0e-2, 4.55349851e-01, -5.68615913e-02,
                                       8.07435011e+01])
-        self.kinematic_NN=kinematicNN()
+        self.kinematic_NN=KinematicNN()
 
     def test_generate_map(self):
         if self.kinematic_NN.SKiNN_installed:

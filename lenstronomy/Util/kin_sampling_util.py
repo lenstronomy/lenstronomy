38b7208d360c5faaae7e56e46ea4b0bb067a1051
mgomer <mgomer@uliege.be>
2023-08-15 18:23:17 +0200

    renamed spectra and imaging coordinate systems

--- lenstronomy/Util/kin_sampling_util.py
+++ lenstronomy/Util/kin_sampling_util.py
@@ -7,24 +7,22 @@ import matplotlib.pyplot as plt
 
 class KinNNImageAlign(object):
     """
-    class to rotate and interpolate SKiNN image (which is built along x-axis) to be on the MUSE grid,
-        rotated to match light distribution as defined by the lens model
-    main function is interp_image() which will output a 2D image interpolated on the MUSE grid
+    class to rotate and interpolate SKiNN image (which is built along x-axis) to be
+        on the grid of the spectral data (e.g., MUSE), rotated to match light distribution as defined by the lens model
+    main function is interp_image() which will output a 2D image interpolated on the spectra grid
     """
 
-    def __init__(self, muse_inputs, hst_inputs, kin_nn_inputs):
+    def __init__(self, spectra_inputs, imaging_inputs, kin_nn_inputs):
         """
         initialize input data
 
-        :param muse_inputs: dictionary which encodes grid and transformation information for kinematic data
-            (doesn't have to be MUSE)
+        :param spectra_inputs: dictionary which encodes grid and transformation information for kinematic data
             :'image': contains 2d image used to calculate grid coordinates
             :'transform_pix2angle': transformation matrix to convert from pixel xy to ra/dec
             :'ra_at_xy0': ra coordinate at pixel (0,0)
             :'dec_at_xy0': dec coordinate at pixel (0,0)
 
-        :param hst_inputs: dictionary which encodes grid and transformation information for imaging data
-            (doesn't have to be HST)
+        :param imaging_inputs: dictionary which encodes grid and transformation information for imaging data
             :'image': contains 2d image used to calculate grid coordinates
             :'transform_pix2angle': transformation matrix to convert from pixel xy to ra/dec
             :'ra_at_xy0': ra coordinate at pixel (0,0)
@@ -38,20 +36,20 @@ class KinNNImageAlign(object):
             :'deltaPix': pixel size
 
         """
-        self.muse_data = muse_inputs
-        self.hst_data = hst_inputs
-        self.hst_deltapix = np.sqrt(np.abs(np.linalg.det(self.hst_data['transform_pix2angle'])))
+        self.spectra_data = spectra_inputs
+        self.imaging_data = imaging_inputs
+        self.imaging_deltapix = np.sqrt(np.abs(np.linalg.det(self.imaging_data['transform_pix2angle'])))
         self.kinNN_data = kin_nn_inputs
         self.write_npix()
 
-    def update(self, muse_inputs=None, hst_inputs=None, kin_nn_inputs=None, update_npix=False):
+    def update(self, spectra_inputs=None, imaging_inputs=None, kin_nn_inputs=None, update_npix=False):
         """
         Update with inputs
         """
-        if muse_inputs is not None:
-            self.muse_data = muse_inputs
-        if hst_inputs is not None:
-            self.hst_data = hst_inputs
+        if spectra_inputs is not None:
+            self.spectra_data = spectra_inputs
+        if imaging_inputs is not None:
+            self.imaging_data = imaging_inputs
         if kin_nn_inputs is not None:
             self.kinNN_data = kin_nn_inputs
         if update_npix:
@@ -61,7 +59,7 @@ class KinNNImageAlign(object):
         """
         Check that images are squared and write the keyword 'npix'
         """
-        for input_set in [self.muse_data, self.hst_data, self.kinNN_data]:
+        for input_set in [self.spectra_data, self.imaging_data, self.kinNN_data]:
             # make sure each image is square and add npix to each dictionary
             if 'image' in input_set.keys():
                 if np.shape(input_set['image'])[0] != np.shape(input_set['image'])[1]:
@@ -73,7 +71,7 @@ class KinNNImageAlign(object):
         """
         simple function to give pixel coordinates of grid
 
-        :param input_set: dictionary from above (e.g. muse_inputs) which completely describes grid transformation
+        :param input_set: dictionary from above (e.g. spectra_inputs) which completely describes grid transformation
         :boolean flatten: default True; if True, return 1D flattened output, if False, return 2D grid
         :return pixel coordinates of grid
         """
@@ -117,19 +115,19 @@ class KinNNImageAlign(object):
         ra, dec = xy_to_radec_matrix.dot(np.array([x, y]))
         return ra + ra_atxy0, dec + dec_atxy0
 
-    def rotate_hst_into_kin_nn(self, hst_x, hst_y, ellipse_pa_to_hstx_angle,
-                               deltapix_hst, deltapix_kin_nn, npix_hst, npix_kin_nn,
-                               offsetx=0, offsety=0):
+    def rotate_imaging_into_kin_nn(self, imaging_x, imaging_y, ellipse_pa_to_imagingx_angle,
+                                   deltapix_imaging, deltapix_kin_nn, npix_imaging, npix_kin_nn,
+                                   offsetx=0, offsety=0):
         """
-        rotates and rescales from the x,y HST coordinate system into the NN coordinate system
+        rotates and rescales from the x,y imaging coordinate system into the NN coordinate system
 
-        :param hst_x: HST x coordinate to transform
-        :param hst_y: HST y coordinate to transform
-        :param ellipse_pa_to_hstx_angle: (radians) position angle of ellipse major axis relative to x
-            in the HST coordinate system
-        :param deltapix_hst: pixel size of HST image
+        :param imaging_x: imaging x coordinate to transform
+        :param imaging_y: imaging y coordinate to transform
+        :param ellipse_pa_to_imagingx_angle: (radians) position angle of ellipse major axis relative to x
+            in the imaging coordinate system
+        :param deltapix_imaging: pixel size of imaging image
         :param deltapix_kin_nn: pixel size of NN image
-        :param npix_hst: number of pixels on a side of the HST image
+        :param npix_imaging: number of pixels on a side of the imaging image
         :param npix_kin_nn: number of pixels on a side of the NN image
         :param offsetx: how many pixels to offset the center of the grid to match the kinNN center (x-direction)
         :param offsety: how many pixels to offset the center of the grid to match the kinNN center (y-direction)
@@ -137,20 +135,20 @@ class KinNNImageAlign(object):
         :return: x and y coordinates in NN coordinate system
         """
         # define rotation matrix to rotate back into alignment
-        counterrotation = -ellipse_pa_to_hstx_angle
+        counterrotation = -ellipse_pa_to_imagingx_angle
         cd1_1 = np.cos(counterrotation)
         cd1_2 = -np.sin(counterrotation)
         cd2_1 = np.sin(counterrotation)
         cd2_2 = np.cos(counterrotation)
         # rotation matrix, applied to matching centers ()
-        rotation_by_ellipse_angle = np.array([[cd1_1, cd1_2], [cd2_1, cd2_2]]) * (deltapix_hst / deltapix_kin_nn)
+        rotation_by_ellipse_angle = np.array([[cd1_1, cd1_2], [cd2_1, cd2_2]]) * (deltapix_imaging / deltapix_kin_nn)
 
-        kin_nn_x_at_hstcenter, kin_nn_y_at_hstcenter = rotation_by_ellipse_angle.dot(
-            np.array([-npix_hst / 2 - offsetx / deltapix_hst, -npix_hst / 2 - offsety / deltapix_hst])) + [
+        kin_nn_x_at_imagingcenter, kin_nn_y_at_imagingcenter = rotation_by_ellipse_angle.dot(
+            np.array([-npix_imaging / 2 - offsetx / deltapix_imaging, -npix_imaging / 2 - offsety / deltapix_imaging])) + [
                                                            npix_kin_nn / 2, npix_kin_nn / 2]
 
-        kin_nn_x, kin_nn_y = rotation_by_ellipse_angle.dot(np.array([hst_x, hst_y]))
-        return kin_nn_x + kin_nn_x_at_hstcenter, kin_nn_y + kin_nn_y_at_hstcenter
+        kin_nn_x, kin_nn_y = rotation_by_ellipse_angle.dot(np.array([imaging_x, imaging_y]))
+        return kin_nn_x + kin_nn_x_at_imagingcenter, kin_nn_y + kin_nn_y_at_imagingcenter
 
     def plot_contour_and_grid(self, xcoords, ycoords, orig_image, color, alpha=0.4):
         """
@@ -167,56 +165,56 @@ class KinNNImageAlign(object):
         plt.scatter(xcoords[ell_cond], ycoords[ell_cond], color=color, alpha=alpha, s=10)
         plt.scatter(xcoords, ycoords, color=color, alpha=alpha, s=1)
 
-    def musegrid_in_radec(self):
+    def spectragrid_in_radec(self):
         """
-        calculates ra and dec coordinates of MUSE input coordinate grid
+        calculates ra and dec coordinates of spectra input coordinate grid
 
         :return: ra and dec coordinates
         """
-        muse_x, muse_y = self.pix_coords(self.muse_data, flatten=True)
-        muse_ra, muse_dec = self.xy_to_radec(muse_x, muse_y, self.muse_data['transform_pix2angle'],
-                                             self.muse_data['ra_at_xy0'], self.muse_data['dec_at_xy0'])
-        return muse_ra, muse_dec
+        spectra_x, spectra_y = self.pix_coords(self.spectra_data, flatten=True)
+        spectra_ra, spectra_dec = self.xy_to_radec(spectra_x, spectra_y, self.spectra_data['transform_pix2angle'],
+                                             self.spectra_data['ra_at_xy0'], self.spectra_data['dec_at_xy0'])
+        return spectra_ra, spectra_dec
 
-    def musegrid_in_hstxy(self):
+    def spectragrid_in_imagingxy(self):
         """
-        calculates x and y coordinates in the HST coordinate system of the original MUSE input grid
-
-        :return: x and y coordinates
-        """
-        muse_ra, muse_dec = self.musegrid_in_radec()
-        muse_coords_in_hst_x, muse_coords_in_hst_y = self.radec_to_xy(muse_ra, muse_dec,
-                                                                      self.hst_data['transform_pix2angle'],
-                                                                      self.hst_data['ra_at_xy0'],
-                                                                      self.hst_data['dec_at_xy0'])
-        return muse_coords_in_hst_x, muse_coords_in_hst_y
-
-    def musegrid_in_kin_nn_xy(self):
-        """
-        calculates x and y coordinates in the NN coordinate system of the original MUSE input grid
+        calculates x and y coordinates in the imaging coordinate system of the original spectra input grid
 
         :return: x and y coordinates
         """
-        muse_coords_in_hst_x, muse_coords_in_hst_y = self.musegrid_in_hstxy()
-        kin_nn_x, kin_nn_y = self.rotate_hst_into_kin_nn(muse_coords_in_hst_x, muse_coords_in_hst_y,
-                                                         self.hst_data['ellipse_PA'], self.hst_deltapix,
-                                                         self.kinNN_data['deltaPix'], self.hst_data['npix'],
-                                                         self.kinNN_data['npix'],
-                                                         offsetx=self.hst_data['offset_x'],
-                                                         offsety=self.hst_data['offset_y'])
+        spectra_ra, spectra_dec = self.spectragrid_in_radec()
+        spectra_coords_in_imaging_x, spectra_coords_in_imaging_y = self.radec_to_xy(spectra_ra, spectra_dec,
+                                                                      self.imaging_data['transform_pix2angle'],
+                                                                      self.imaging_data['ra_at_xy0'],
+                                                                      self.imaging_data['dec_at_xy0'])
+        return spectra_coords_in_imaging_x, spectra_coords_in_imaging_y
+
+    def spectragrid_in_kin_nn_xy(self):
+        """
+        calculates x and y coordinates in the NN coordinate system of the original spectra input grid
+
+        :return: x and y coordinates
+        """
+        spectra_coords_in_imaging_x, spectra_coords_in_imaging_y = self.spectragrid_in_imagingxy()
+        kin_nn_x, kin_nn_y = self.rotate_imaging_into_kin_nn(spectra_coords_in_imaging_x, spectra_coords_in_imaging_y,
+                                                             self.imaging_data['ellipse_PA'], self.imaging_deltapix,
+                                                             self.kinNN_data['deltaPix'], self.imaging_data['npix'],
+                                                             self.kinNN_data['npix'],
+                                                             offsetx=self.imaging_data['offset_x'],
+                                                             offsety=self.imaging_data['offset_y'])
         return kin_nn_x, kin_nn_y
 
     def interp_image(self):
         """
-        interpolates kinNN image at the coordinates of the transformed MUSE grid
+        interpolates kinNN image at the coordinates of the transformed spectra grid
 
-        :return: interpolated image which lines up with MUSE coordinates
+        :return: interpolated image which lines up with spectra coordinates
         """
-        muse_kin_nn_x, muse_kin_nn_y = self.musegrid_in_kin_nn_xy()
+        spectra_kin_nn_x, spectra_kin_nn_y = self.spectragrid_in_kin_nn_xy()
         x_axis = np.arange(self.kinNN_data['npix'])
         y_axis = np.arange(self.kinNN_data['npix'])
         interp_fcn = RectBivariateSpline(x_axis, y_axis, self.kinNN_data['image'])
         # y and x are flipped in RectBivariateSpline call:
-        flux_interp = interp_fcn.ev(muse_kin_nn_y, muse_kin_nn_x).reshape(self.muse_data['npix'],
-                                                                          self.muse_data['npix'])
+        flux_interp = interp_fcn.ev(spectra_kin_nn_y, spectra_kin_nn_x).reshape(self.spectra_data['npix'],
+                                                                          self.spectra_data['npix'])
         return flux_interp
--- test/test_Util/test_kin_sampling_util.py
+++ test/test_Util/test_kin_sampling_util.py
@@ -12,67 +12,67 @@ class TestKinSamplingUtil(object):
 
         transform_pix2angle = np.array([[1, 0], [0, 1]])  # linear translation matrix of no rotation
         ra_at_xy_0, dec_at_xy_0 = transform_pix2angle.dot([-5 / 2, -5 / 2]).T
-        self.kwargs_pixel_rot_hst = {'nx': 5, 'ny': 5,  # number of pixels per axis
+        self.kwargs_pixel_rot_imaging = {'nx': 5, 'ny': 5,  # number of pixels per axis
                                      'ra_at_xy_0': ra_at_xy_0,  # RA at pixel (0,0)
                                      'dec_at_xy_0': dec_at_xy_0,  # DEC at pixel (0,0)
                                      'transform_pix2angle': transform_pix2angle}
-        self.hst_inputs = {'image': self.image, 'transform_pix2angle': self.kwargs_pixel_rot_hst['transform_pix2angle'],
-                           'ra_at_xy0': self.kwargs_pixel_rot_hst['ra_at_xy_0'],
-                           'dec_at_xy0': self.kwargs_pixel_rot_hst['dec_at_xy_0'],
+        self.imaging_inputs = {'image': self.image, 'transform_pix2angle': self.kwargs_pixel_rot_imaging['transform_pix2angle'],
+                           'ra_at_xy0': self.kwargs_pixel_rot_imaging['ra_at_xy_0'],
+                           'dec_at_xy0': self.kwargs_pixel_rot_imaging['dec_at_xy_0'],
                            'ellipse_PA': 0,
                            'offset_x': 0, 'offset_y': 0}
         self.kinNN_inputs = {'image': self.image, 'deltaPix': 1}
 
-    def define_muse_kwargs_from_rotation_angle(self, rotation_angle):
+    def define_spectra_kwargs_from_rotation_angle(self, rotation_angle):
         transform_pix2angle = np.array([[np.cos(rotation_angle), -np.sin(rotation_angle)], [np.sin(rotation_angle),
                                                                                             np.cos(
                                                                                                 rotation_angle)]])  # linear translation matrix of a 90 degree rotation
         ra_at_xy_0, dec_at_xy_0 = transform_pix2angle.dot([-5 / 2, -5 / 2]).T
-        kwargs_pixel_rot_muse = {'nx': 5, 'ny': 5,  # number of pixels per axis
+        kwargs_pixel_rot_spectra = {'nx': 5, 'ny': 5,  # number of pixels per axis
                                  'ra_at_xy_0': ra_at_xy_0,  # RA at pixel (0,0)
                                  'dec_at_xy_0': dec_at_xy_0,  # DEC at pixel (0,0)
                                  'transform_pix2angle': transform_pix2angle}
-        muse_inputs = {'image': self.image, 'transform_pix2angle': kwargs_pixel_rot_muse['transform_pix2angle'],
-                       'ra_at_xy0': kwargs_pixel_rot_muse['ra_at_xy_0'],
-                       'dec_at_xy0': kwargs_pixel_rot_muse['dec_at_xy_0']}
-        return muse_inputs
+        spectra_inputs = {'image': self.image, 'transform_pix2angle': kwargs_pixel_rot_spectra['transform_pix2angle'],
+                       'ra_at_xy0': kwargs_pixel_rot_spectra['ra_at_xy_0'],
+                       'dec_at_xy0': kwargs_pixel_rot_spectra['dec_at_xy_0']}
+        return spectra_inputs
 
     def test_rotation(self):
-        # muse grid not rotated from HST:
-        muse_inputs = self.define_muse_kwargs_from_rotation_angle(0)
-        skinn_align = kin_sampling_util.KinNNImageAlign(muse_inputs, self.hst_inputs, self.kinNN_inputs)
-        muse_img_coords = skinn_align.musegrid_in_hstxy()
-        hst_img_coords = skinn_align.pix_coords(self.hst_inputs)
-        npt.assert_allclose(hst_img_coords, muse_img_coords, atol=1e-7)
+        # spectra grid not rotated from imaging:
+        spectra_inputs = self.define_spectra_kwargs_from_rotation_angle(0)
+        skinn_align = kin_sampling_util.KinNNImageAlign(spectra_inputs, self.imaging_inputs, self.kinNN_inputs)
+        spectra_img_coords = skinn_align.spectragrid_in_imagingxy()
+        imaging_img_coords = skinn_align.pix_coords(self.imaging_inputs)
+        npt.assert_allclose(imaging_img_coords, spectra_img_coords, atol=1e-7)
 
-        muse_inputs = self.define_muse_kwargs_from_rotation_angle(2 * np.pi)
-        skinn_align = kin_sampling_util.KinNNImageAlign(muse_inputs, self.hst_inputs, self.kinNN_inputs)
-        muse_img_coords = skinn_align.musegrid_in_hstxy()
-        hst_img_coords = skinn_align.pix_coords(self.hst_inputs)
-        npt.assert_allclose(hst_img_coords, muse_img_coords, atol=1e-7)
+        spectra_inputs = self.define_spectra_kwargs_from_rotation_angle(2 * np.pi)
+        skinn_align = kin_sampling_util.KinNNImageAlign(spectra_inputs, self.imaging_inputs, self.kinNN_inputs)
+        spectra_img_coords = skinn_align.spectragrid_in_imagingxy()
+        imaging_img_coords = skinn_align.pix_coords(self.imaging_inputs)
+        npt.assert_allclose(imaging_img_coords, spectra_img_coords, atol=1e-7)
 
-        # muse_grid 90 degrees from HST:
-        muse_inputs = self.define_muse_kwargs_from_rotation_angle(np.pi / 2)
-        skinn_align = kin_sampling_util.KinNNImageAlign(muse_inputs, self.hst_inputs, self.kinNN_inputs)
-        muse_img_coords = skinn_align.musegrid_in_hstxy()
-        hst_img_coords = skinn_align.pix_coords(self.hst_inputs)
-        npt.assert_allclose(hst_img_coords[0], muse_img_coords[1], atol=1e-7)  # old x coordinate is new y coordinate
+        # spectra_grid 90 degrees from imaging:
+        spectra_inputs = self.define_spectra_kwargs_from_rotation_angle(np.pi / 2)
+        skinn_align = kin_sampling_util.KinNNImageAlign(spectra_inputs, self.imaging_inputs, self.kinNN_inputs)
+        spectra_img_coords = skinn_align.spectragrid_in_imagingxy()
+        imaging_img_coords = skinn_align.pix_coords(self.imaging_inputs)
+        npt.assert_allclose(imaging_img_coords[0], spectra_img_coords[1], atol=1e-7)  # old x coordinate is new y coordinate
 
     def test_samegrid(self):
         # check interpolation in case where grids are the same
-        muse_inputs = self.define_muse_kwargs_from_rotation_angle(0)
-        skinn_align = kin_sampling_util.KinNNImageAlign(muse_inputs, self.hst_inputs, self.kinNN_inputs)
+        spectra_inputs = self.define_spectra_kwargs_from_rotation_angle(0)
+        skinn_align = kin_sampling_util.KinNNImageAlign(spectra_inputs, self.imaging_inputs, self.kinNN_inputs)
         interp_image = skinn_align.interp_image()
         npt.assert_almost_equal(interp_image, self.image)
 
     def test_offset(self):
-        hst_inputs = {'image': self.image, 'transform_pix2angle': self.kwargs_pixel_rot_hst['transform_pix2angle'],
-                      'ra_at_xy0': self.kwargs_pixel_rot_hst['ra_at_xy_0'],
-                      'dec_at_xy0': self.kwargs_pixel_rot_hst['dec_at_xy_0'],
+        imaging_inputs = {'image': self.image, 'transform_pix2angle': self.kwargs_pixel_rot_imaging['transform_pix2angle'],
+                      'ra_at_xy0': self.kwargs_pixel_rot_imaging['ra_at_xy_0'],
+                      'dec_at_xy0': self.kwargs_pixel_rot_imaging['dec_at_xy_0'],
                       'ellipse_PA': 0,
                       'offset_x': 1, 'offset_y': 1}
-        muse_inputs = self.define_muse_kwargs_from_rotation_angle(0)
-        skinn_align = kin_sampling_util.KinNNImageAlign(muse_inputs, hst_inputs, self.kinNN_inputs)
+        spectra_inputs = self.define_spectra_kwargs_from_rotation_angle(0)
+        skinn_align = kin_sampling_util.KinNNImageAlign(spectra_inputs, imaging_inputs, self.kinNN_inputs)
         interp_image = skinn_align.interp_image()
         # #uncomment to see plots
         # plt.imshow(image)

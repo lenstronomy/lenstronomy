6e4e32ea67e97f071939a785b5acd19a8e3f9913
dangilman <gilmanda@ucla.edu>
2023-11-18 11:20:40 -0600

    Update profile_list_base.py

--- lenstronomy/LensModel/profile_list_base.py
+++ lenstronomy/LensModel/profile_list_base.py
@@ -971,6 +971,12 @@ def lens_class(
         from lenstronomy.LensModel.Profiles.nfw_core_truncated import TNFWC
 
         return TNFWC()
+    elif lens_type == "EPL_MULTIPOLE_M3M4":
+            from lenstronomy.LensModel.Profiles.epl_multipole_m3m4 import (
+                EPL_MULTIPOLE_M3M4,
+            )
+
+            return EPL_MULTIPOLE_M3M4()
     else:
         raise ValueError(
             "%s is not a valid lens model. Supported are: %s."

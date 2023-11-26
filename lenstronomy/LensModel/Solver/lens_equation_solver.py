3f17af6543b69fc8dfabb0db66927cb1a4504523
Anowar Shajib <ajshajib@uchicago.edu>
2023-11-25 21:25:26 -0600

    Update lenstronomy/LensModel/Solver/lens_equation_solver.py

--- lenstronomy/LensModel/Solver/lens_equation_solver.py
+++ lenstronomy/LensModel/Solver/lens_equation_solver.py
@@ -613,7 +613,7 @@ class LensEquationSolver(object):
 
         if hasattr(self.lensModel, "_no_potential"):
             raise Exception(
-                "Instance of lens_model passed to this class does not compute the lensing potential, "
+                "Instance of `LensModel` passed to this class does not compute the lensing potential, "
                 "and therefore cannot compute time delays."
             )
 

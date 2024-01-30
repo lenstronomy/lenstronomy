9068697ddbffe1030d166ae383f3e675b2af4b4a
Anowar Shajib <ajshajib@uchicago.edu>
2024-01-29 23:42:27 -0600

    Update test/test_Workflow/test_alignment_matching.py

--- test/test_Workflow/test_alignment_matching.py
+++ test/test_Workflow/test_alignment_matching.py
@@ -13,7 +13,7 @@ from lenstronomy.Data.psf import PSF
 
 
 class TestAlignmentMatching(object):
-    def setup_method(self):
+    def __init__(self):
         np.random.seed(41)
         # data specifics
         sigma_bkg = 0.01  # background noise per pixel

8501312d7c9f647330e905b8f807d23cafd8ef2e
Anowar Shajib <ajshajib@uchicago.edu>
2024-01-29 23:23:15 -0600

    Update test/test_Data/test_kinematic_data_2D.py

--- test/test_Data/test_kinematic_data_2D.py
+++ test/test_Data/test_kinematic_data_2D.py
@@ -11,7 +11,7 @@ import lenstronomy.Util.kernel_util as kernel_util
 
 
 class TestKinBin(object):
-    def setup_method(self):
+    def __init__(self):
         self.numPix = 10
         self.num_bin = 4
         kwargs_kin = {

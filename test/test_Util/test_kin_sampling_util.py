54b927ef1da1f10b6ac4352e6008f84d1442d98e
Anowar Shajib <ajshajib@uchicago.edu>
2024-01-30 10:33:48 -0600

    Update test/test_Util/test_kin_sampling_util.py

--- test/test_Util/test_kin_sampling_util.py
+++ test/test_Util/test_kin_sampling_util.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 class TestKinSamplingUtil(object):
-    def __init__(self):
+    def setup_method(self, method):
         self.image = np.array(
             [
                 [0, 1, 0, 0, 0],

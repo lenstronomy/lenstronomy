9edd87581cd1bac3f2d2351cc49283500f3a0648
Alex Landry <avlando645@gmail.com>
2024-04-22 04:20:35 -0400

    changed names in test to new function name

--- test/test_LensModel/test_Microlensing/test_ABM.py
+++ test/test_LensModel/test_Microlensing/test_ABM.py
@@ -4,12 +4,13 @@ from Microlensing.adaptive_boundary_mesh import loop_information
 from Microlensing.adaptive_boundary_mesh import within_distance
 from Microlensing.adaptive_boundary_mesh import adaptive_boundary_mesh
 
-
 class TestABM:
 
     def test_loop_information(self):
-        """Function to test the loop information functionality."""
-
+        """
+        Function to test the loop information functionality.
+        """
+        
         eta = 2
         beta_0 = 50
         beta_s = 10
@@ -32,48 +33,31 @@ class TestABM:
         assert np.all(distances_within)
 
         # Test points not within the threshold
-        distances_not_within = within_distance(
-            center_points_not_within, test_point, threshold
-        )
+        distances_not_within = within_distance(center_points_not_within, test_point, threshold)
         assert not np.any(distances_not_within)
 
     def test_ABM(self):
-        """Test function for ABM.
-
-        It sets up the initial parameters and checks if the function returns the
-        expected results.
         """
-
+        Test function for ABM. It sets up the initial parameters and checks if the function returns the expected results.
+        """
+        
         source_position = (0, 0)
         L = 11
         beta_0 = 50
         beta_s = 10
         n_p = 10
         eta = 2
-        number_of_iterations = 5
+        number_of_iterations =  5
         final_eta = 1.25
-        kwargs_lens = [{"theta_E": 10, "center_x": 2, "center_y": 3}]
+        kwargs_lens = [{'theta_E': 10, 'center_x': 2, 'center_y': 3}]
 
-<<<<<<< HEAD
         subset_centers, side_length, total_number_of_rays_shot = adaptive_boundary_mesh(source_position, L, beta_0, beta_s, n_p, eta, number_of_iterations, final_eta, kwargs_lens)
-=======
-        subset_centers, side_length, total_number_of_rays_shot = ABM(
-            source_position,
-            L,
-            beta_0,
-            beta_s,
-            n_p,
-            eta,
-            number_of_iterations,
-            final_eta,
-            kwargs_lens,
-        )
->>>>>>> 31776603c7dc39d113b800e8628375123dffb01a
 
         expected_number_of_subset_centers = 26071637
         expected_side_length = 0.0011
-        expected_total_number_of_rays_shot = 26334987
+        expected_total_number_of_rays_shot= 26334987
 
         assert len(subset_centers) == expected_number_of_subset_centers
         assert expected_side_length == side_length
         assert total_number_of_rays_shot == expected_total_number_of_rays_shot
+

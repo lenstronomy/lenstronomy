a967827791a722e256adb869f6a1137989a5eee9
Michael Martinez <mnmartinez@wisc.edu>
2024-05-20 10:31:28 -0500

    More elaborate documentation, slight flux calculation speedup, changed name in light_model_base.py from 'LINEPROFILE' to 'LINE_PROFILE'

--- lenstronomy/LightModel/Profiles/lineprofile.py
+++ lenstronomy/LightModel/Profiles/lineprofile.py
@@ -6,7 +6,11 @@ __all__ = ["LineProfile"]
 
 class LineProfile(object):
     """
-    Horizontal line segment class.
+    Horizontal line segment class. The line extends `length` arcseconds from 
+    (`start_x`, `start_y`) at an angle `angle` degrees to the horizontal. Line `width`
+    is centered in the perpendicular direction, e.g. a profile with 1 arcsecond width
+    and `angle=0` will span -0.5 to 0.5 in the y-direction. Surface brightness is 
+    constant and given by `amp`.
     """
 
     param_names = ["amp", "angle", "length", "width", "start_x", "start_y"]
@@ -45,7 +49,10 @@ class LineProfile(object):
         ang = -np.deg2rad(angle)
         x_ = np.cos(ang)*(start_x - x) + np.sin(ang)*(start_y - y)
         y_ = np.cos(ang)*(start_y - y) - np.sin(ang)*(start_x - x)
-        return amp * (x_ > 0) * (x_ < length) * (abs(y_) < width/2)
+        flux = np.zeros_like(x_)
+        flux[(x_ > 0) * (x_ < length) * (abs(y_) < width/2)] = amp
+        return flux
+
 
     def total_flux(self, amp, angle, length, width, start_x=0, start_y=0):
         """Integrated flux of the profile.

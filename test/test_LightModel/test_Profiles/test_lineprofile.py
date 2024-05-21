Added lineProfile to linear_basis, updated test_num_param_linear to 20 components, redid sign error fix from previous commit

test/test_LightModel/test_Profiles/test_lineprofile.py
@@ -12,10 +12,10 @@ class TestLineProfile(object):
         length = 1
         width = 0.01
         angle = 57
-        x = np.array([0, 1, 0.5 * np.cos(np.deg2rad(angle))])
-        y = np.array([0, 0, 0.5 * np.sin(np.deg2rad(angle))])
-        x_single = 0.2 * np.cos(np.deg2rad(angle))
-        y_single = 0.2 * np.sin(np.deg2rad(angle))
+        x = np.array([0, 1, 0.5 * np.cos(np.deg2rad(-angle))])
+        y = np.array([0, 0, 0.5 * np.sin(np.deg2rad(-angle))])
+        x_single = 0.2 * np.cos(np.deg2rad(-angle))
+        y_single = 0.2 * np.sin(np.deg2rad(-angle))
         flux_true = np.array([amp, 0, amp])
         flux = self.lineprofile.function(x, y, amp, angle, length, width)
         single_flux_true = amp
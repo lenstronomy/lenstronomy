6ab2d21868245efbd5a3f7176fa7bcf7702545e0
Simon Birrer <sibirrer@users.noreply.github.com>
2024-05-25 11:31:42 -0400

    added documentation

--- lenstronomy/LightModel/Profiles/hernquist.py
+++ lenstronomy/LightModel/Profiles/hernquist.py
@@ -5,7 +5,7 @@ __all__ = ["Hernquist", "HernquistEllipse"]
 
 
 class Hernquist(object):
-    """Class for pseudo Jaffe lens light (2d projected light/mass distribution."""
+    """Class for Hernquist lens light (2d projected light/mass distribution)."""
 
     def __init__(self):
         from lenstronomy.LensModel.Profiles.hernquist import Hernquist as Hernquist_lens
@@ -28,13 +28,13 @@ class Hernquist(object):
     def function(self, x, y, amp, Rs, center_x=0, center_y=0):
         """
 
-        :param x:
-        :param y:
-        :param amp:
+        :param x: x-position
+        :param y: y-position
+        :param amp: surface brightness amplitude
         :param Rs: scale radius: half-light radius = Rs / 0.551
-        :param center_x:
-        :param center_y:
-        :return:
+        :param center_x: centroid in x-direction
+        :param center_y: centroid in y-direction
+        :return: surface brightness
         """
         rho0 = self.lens.sigma2rho(amp, Rs)
         return self.lens.density_2d(x, y, rho0, Rs, center_x, center_y)
@@ -42,9 +42,9 @@ class Hernquist(object):
     def light_3d(self, r, amp, Rs):
         """
 
-        :param r:
-        :param amp:
-        :param Rs:
+        :param r: 3d radius (in angular units)
+        :param amp: surface brightness amplitude
+        :param Rs: scale radius: half-light radius = Rs / 0.551
         :return:
         """
         rho0 = self.lens.sigma2rho(amp, Rs)
@@ -54,9 +54,9 @@ class Hernquist(object):
     def total_flux(amp, Rs):
         """
 
-        :param amp:
-        :param Rs:
-        :return:
+        :param amp: surface brightness amplitude
+        :param Rs: scale radius: half-light radius = Rs / 0.551
+        :return: total integrated flux of profile
         """
         rhos = amp / Rs
         m_tot = 2 * np.pi * rhos * Rs ** 3
@@ -94,15 +94,15 @@ class HernquistEllipse(object):
     def function(self, x, y, amp, Rs, e1, e2, center_x=0, center_y=0):
         """
 
-        :param x:
-        :param y:
-        :param amp:
-        :param Rs:
-        :param e1:
-        :param e2:
-        :param center_x:
-        :param center_y:
-        :return:
+        :param x: x-position
+        :param y: y-position
+        :param amp: surface brightness amplitude
+        :param Rs: scale radius: half-light radius = Rs / 0.551
+        :param e1: eccentricity component
+        :param e2: eccentricity component
+        :param center_x: centroid in x-direction
+        :param center_y: centroid in y-direction
+        :return: surface brightness
         """
         x_, y_ = param_util.transform_e1e2_product_average(
             x, y, e1, e2, center_x, center_y
@@ -112,12 +112,12 @@ class HernquistEllipse(object):
     def light_3d(self, r, amp, Rs, e1=0, e2=0):
         """
 
-        :param r:
-        :param amp:
-        :param Rs:
-        :param e1:
-        :param e2:
-        :return:
+        :param r: 3d radius (in angular units)
+        :param amp: surface brightness amplitude
+        :param Rs: scale radius: half-light radius = Rs / 0.551
+        :param e1: eccentricity component
+        :param e2: eccentricity component
+        :return: flux density in 3d
         """
         rho0 = self.lens.sigma2rho(amp, Rs)
         return self.lens.density(r, rho0, Rs)
@@ -125,10 +125,10 @@ class HernquistEllipse(object):
     def total_flux(self, amp, Rs, e1=0, e2=0):
         """
 
-        :param amp:
-        :param Rs:
-        :param e1:
-        :param e2:
-        :return:
+        :param amp: surface brightness amplitude
+        :param Rs: scale radius: half-light radius = Rs / 0.551
+        :param e1: eccentricity component
+        :param e2: eccentricity component
+        :return: total integrated flux
         """
         return self.spherical.total_flux(amp=amp, Rs=Rs)

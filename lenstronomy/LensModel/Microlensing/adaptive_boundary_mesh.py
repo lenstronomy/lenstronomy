ac4db9c848374ea8239b526606d2f3467974e67c
Alex Landry <avlando645@gmail.com>
2024-06-17 03:40:43 -0400

    updated unit tests and docs for updated ABM algorithm

--- lenstronomy/LensModel/Microlensing/adaptive_boundary_mesh.py
+++ lenstronomy/LensModel/Microlensing/adaptive_boundary_mesh.py
@@ -1,113 +1,77 @@
+from lenstronomy.Cosmo.micro_lensing import einstein_radius, source_size
+from lenstronomy.LensModel.lens_model import LensModel
+import matplotlib.pyplot as plt
 import numpy as np
 import math
-from lenstronomy.LensModel.lens_model import LensModel
-from lenstronomy.Cosmo.micro_lensing import einstein_radius, source_size
 
+def splitting_centers(x, y, side_length, n_p):
 
-def splitting_centers(center, side_length, n_p):
-    """Takes square centered at center = (x, y) with side_length = side length.
-
-    (float) and divides it into n_p (integer) squares along each axis - so
-    returns n_p * n_p subsquares from the original square. In particular,
-    the function returns the coordinates of the centers of these subsquares,
-    along with the final side length of the subsquare.
-
-    :param center: center of square
-    :type center: nparray
+    """Takes square centered at center = (x, y) with side_length = side length 
+       (float) and divides it into n_p (integer) squares along each axis - so 
+       returns n_p * n_p subsquares from the original square. In particular, 
+       the function returns the coordinates of the centers of these subsquares,
+       along with the final side length of the subsquare.
+       
+    :param x: x coordinate of center of square
+    :type x: nparray
+    :param y: y coordinate of center of square
+    :type y: nparray
     :param side_length: side length of square
     :type side_length: float
     :param n_p: number of squares along each axis
     :type n_p: int
-    :return: coordinates of centers of subsquares
+    :return: x and y coordinates of centers of subsquares
     :rtype: nparray
-    """
+    :return: side length of subsquares
+    :rtype: float
+       """
 
-    center_x = center[:, 0]
-    center_y = center[:, 1]
-
-    new_x = np.empty(n_p**2 * len(center_x))
-    new_y = np.empty(n_p**2 * len(center_y))
+    new_x = np.empty(n_p**2 * len(x))
+    new_y = np.empty(n_p**2 * len(y))
 
     k = 0
     for i in range(n_p):
         for j in range(n_p):
-            new_x[k :: n_p**2] = (
-                center_x - side_length / 2 + (j + 0.5) * side_length / n_p
-            )
-            new_y[k :: n_p**2] = (
-                center_y - side_length / 2 + (i + 0.5) * side_length / n_p
-            )
+            new_x[k::n_p**2] = x - side_length / 2 + (j + 0.5) * side_length / n_p
+            new_y[k::n_p**2] = y - side_length / 2 + (i + 0.5) * side_length / n_p
             k += 1
 
-    centers = np.column_stack((new_x, new_y))
     new_side_length = side_length / n_p
-    return centers, new_side_length
+    return new_x, new_y, new_side_length
 
-
+#defines loop_information to defines number of iterations and final scale factor  
 def loop_information(eta, beta_0, beta_s):
-    """# Defines loop_information to defines number of iterations and final scale factor
-
-    :param eta: 0.7 * n_p
-    :type eta: float
-    :param beta_0: Initial search radius (delta_beta)
-    :type beta_0: float
-    :param beta_s: Factor of 1/2 because radius
-    :type beta_s: float
-    :return: number_of_iterations: Number of iterations
-    :rtype: number_of_iterations: int
-    :return: final_eta: Final scale factor
-    :rtype: final_eta: float
-    """
+    
     N = 1 + math.log((beta_0 / beta_s), eta)
     number_of_iterations = math.ceil(N)
     N_star = N - math.floor(N)
-    final_eta = eta**N_star
+    final_eta = eta ** N_star 
 
     return number_of_iterations, final_eta
 
+def within_distance(x, y, test_point, threshold, threshold_2):
+    """
+    Check if points in center_points are within a threshold distance of the test_point, component-wise.
 
-def within_distance(center_points, test_point, threshold):
-    """Check if points in center_points are within a threshold distance of the
-    test_point.
-
-    :param center_points: Array of center points, each row containing (x, y)
-        coordinates. Source coordrates of grid
-    :type center_points: nparray
+    :param x: x source coordrate of grid
+    :type x: float
+    :param y: y source coordrate of grid
+    :type y: float
     :param test_point: Coordinates of the test point (x, y). Source position
     :type test_point: nparray
-    :param threshold: Distance threshold.
+    :param threshold: delta_beta value.
     :type threshold: float
-    :return: Boolean array indicating whether each point is within the threshold
-        distance.
+    :param threshold_2: beta_s value.
+    :type threshold: float
+    :return: Boolean array indicating whether each point is within both threshold distances.
     :rtype: nparray
     """
 
-    center_points_x = center_points[:, 0]
-    center_points_y = center_points[:, 1]
-    test_point_x = test_point[0]
-    test_point_y = test_point[1]
-    distances = np.sqrt(
-        (center_points_x - test_point_x) ** 2 + (center_points_y - test_point_y) ** 2
-    )
-    return distances < threshold
+    distances = ((x - test_point[0])**2 + (y - test_point[1])**2)**(1/2)
+    return (distances < threshold) & (distances < threshold_2)
 
-
-# temporary function name, basically its the ABM algorithm with pixel division
-def adaptive_boundary_mesh(
-    source_position,
-    L,
-    beta_0,
-    beta_s,
-    n_p,
-    eta,
-    number_of_iterations,
-    final_eta,
-    kwargs_lens,
-):
-    """Iterative adaptive process based on Meena et al.
-
-    (2022): https://arxiv.org/abs/2203.08131
-    """
+#temporary function name, basically its the ABM algorithm with pixel division
+def adaptive_boundary_mesh(source_position, L, beta_0, beta_s, n_p, eta, number_of_iterations, final_eta, kwargs_lens):
 
     """
     Returns list of those high resolution image-plane pixels that were 
@@ -126,70 +90,77 @@ def adaptive_boundary_mesh(
     :type beta_s: float
     :param n_p: Number of pixels
     :type n_p: int
-    :param eta: 0.7 * n_p
-    :type eta: float
+    :param eta: Factor by which the search radius is reduced in each iteration
+   :type eta: float
     :param number_of_iterations: Number of iterations
     :type number_of_iterations: int
     :param final_eta: Final scale factor
     :type final_eta: float
     :param kwargs_lens: Keyword arguments for lens model
     :type kwargs_lens: dict
-    return: subset_centers: List of high resolution image-plane pixels that were mapped to within the radius β_s around the source position (β1, β2) in the source plane
-    :rtype: subset_centers: nparray
     return: side_length: updated side length of square area in image plane
     :rtype: side_length: nparray
     return: total_number_of_rays_shot: total number of rays shot
     :rtype: total_number_of_rays_shot: int
+    return: subset_image_centers_x, subset_image_centers_y: x and y coordinates of high resolution image-plane pixels that were mapped to within the radius β_s around the source position (β1, β2) in the source plane
+    :rtype: subset_image_centers_x, subset_image_centers_y: nparray
     """
-
     # default values
-    # d_l = 4000  # distance of the lens in pc
-    # d_s = 8000  # distance of the source in pc
-    # M0 = 0.01 # mass of the lens in units of M_sol (limited to 0.1 M_sol)
-    # diameter_s = 20 # size of the diameter of the source star in units of the solar radius
-    # theta_E = einstein_radius(M0, d_l, d_s)
-    # size_s = source_size(diameter_s, d_s)
-    # L = theta_E * 4 # side length of square area in image plane
-    # beta_0 = 4 * L # initial search radius (delta_beta) - few times bigger than "necessary" to be safe (delta_beta)
-    # beta_s = size_s / 2 # factor of 1/2 because radius
-    # n_p = 30
-    # eta = 0.7 * n_p
-    # kwargs_lens = [{'theta_E': theta_E, 'center_x': theta_E / 4, 'center_y': theta_E / 6}]
-    # source_position = (0, 0)
+
+    # define the microlens
+    d_l = 4000  # distance of the lens in pc
+    d_s = 8000  # distance of the source in pc
+    M0 = 0.01 # mass of the lens in units of M_sol (limited up to 0.25 M_sol)
+    diameter_s = 20 # size of the diameter of the source star in units of the solar radius
+    theta_E = einstein_radius(M0, d_l, d_s) # returns Einstein radius in arcseconds
+    source_diameter = source_size(diameter_s, d_s) #converts source star diameter from solar radius to arcseconds
+
+    lens = LensModel(lens_model_list=['POINT_MASS'])
+    kwargs_lens = [{'theta_E': theta_E, 'center_x': theta_E / 4, 'center_y': theta_E / 6}]
+
+    # define square area and search radius
+    L = theta_E * 4 # side length of square area in image plane - same as lenstronomy grid width, arc seconds
+    beta_0 = 4*L # initial search radius few times bigger than "necessary" to be safe, arc seconds
+    beta_s = source_diameter / 2 # source size, factor of 1/2 because radius, arc seconds
+    n_p = 5 # number of of subsquares per side by which each (valid) pixel is divided in the next iteration
+    eta = 0.7 * n_p # the factor by which the search radius is reduced in each iteration
+    source_position = (0, 0) # position of the source in the source plane
+
+    # creates an loop_info array to store number of iterations and final scale factor
+    loop_info = loop_information(eta, beta_0, beta_s)
+    number_of_iterations = loop_info[0]
+    final_eta = loop_info[1]
 
     # Initialize variables
     total_number_of_rays_shot = 0  # Counter for total number of rays shot
     i = 1  # Iteration counter
-    centers = np.array([[0, 0]])  # Initial center coordinates
-    side_length = L  # Initial side length of square region (for source image)
-    delta_beta = beta_0  # Initial step size for source plane radius
-    lens = LensModel(lens_model_list=["POINT_MASS"])
-
+    image_centers = np.array([[0, 0]])  # Initial center coordinates
+    image_centers_x, image_centers_y = image_centers[:, 0], image_centers[:, 1]
+    side_length = L # Initial side length of square region (for source image), same as image plane side length, arc seconds
+    delta_beta = beta_0 # Initial step size for source plane radius, arc seconds
+    
     # Main loop for adaptive boundary mesh algorithm
     while i < number_of_iterations:
 
         # Split image plane centers
-        centers = splitting_centers(centers, side_length, n_p)[0]
+        image_centers_x, image_centers_y, _ = splitting_centers(image_centers_x, image_centers_y, side_length, n_p)
 
         # Ray shoot from image to source plane using array-based approach
-        source_coords_x, source_coords_y = lens.ray_shooting(
-            centers[:, 0], centers[:, 1], kwargs=kwargs_lens
-        )
+        source_centers_x, source_centers_y = lens.ray_shooting(image_centers_x, image_centers_y, kwargs=kwargs_lens)
+        
+        within_radius = within_distance(source_centers_x, source_centers_y, source_position, delta_beta, beta_s)
+        subset_image_centers_x = image_centers_x[within_radius]
+        subset_image_centers_y = image_centers_y[within_radius]
 
-        # Calculate source_coords array
-        source_coords = np.column_stack((source_coords_x, source_coords_y))
+        # Update total number of rays shot
+        total_number_of_rays_shot += len(subset_image_centers_x)
 
-        # Define within_radius
-        within_radius = within_distance(source_coords, source_position, beta_s)
-
-        # Collect subset of centers in image plane
-        subset_centers = centers[within_radius]
-
-        total_number_of_rays_shot += len(subset_centers)
+        # Update centers to be matched image centers with subset of centers in source plane
+        combined_images = np.column_stack((subset_image_centers_x, subset_image_centers_y)) #not used in calculation, used for debugging
 
         # Update side length
         side_length /= n_p
-
+        
         # Update delta_beta based on iteration number
         if i < number_of_iterations:
             delta_beta /= eta
@@ -198,5 +169,5 @@ def adaptive_boundary_mesh(
 
         # Increment iteration counter
         i += 1
-
-    return subset_centers, side_length, total_number_of_rays_shot
+    
+    return side_length, total_number_of_rays_shot, subset_image_centers_x, subset_image_centers_y
---
+++ test/test_LensModel/test_Microlensing/test_adaptive_boundary_mesh.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pytest
+from lenstronomy.LensModel.Microlensing.adaptive_boundary_mesh import splitting_centers
+from lenstronomy.LensModel.Microlensing.adaptive_boundary_mesh import loop_information
+from lenstronomy.LensModel.Microlensing.adaptive_boundary_mesh import within_distance
+from lenstronomy.LensModel.Microlensing.adaptive_boundary_mesh import adaptive_boundary_mesh
+
+class Test_adaptive_boundary_mesh:
+
+    def test_splitting_centers(self):
+        """
+        Function to test the splitting centers functionality.
+        """
+        
+        center = np.array([[0, 0]])
+        x = center[:, 0]
+        y = center[:, 1]
+        side_length = 1
+        n_p = 2
+        expected_x = np.array([-0.25, 0.25,- 0.25, 0.25])
+        expected_y = np.array([-0.25, -0.25, 0.25, 0.25])
+
+        new_x, new_y, new_side_length = splitting_centers(x, y, side_length, n_p)
+        assert np.all(new_x == expected_x)
+        assert np.all(new_y == expected_y)
+        assert new_side_length == pytest.approx(0.5, rel=1e-7)
+
+    def test_loop_information(self):
+        """
+        Function to test the loop information functionality.
+        """
+        
+        eta = 2
+        beta_0 = 50
+        beta_s = 10
+        expected_iterations = 4
+        expected_final_eta = 1.25
+
+        number_of_iterations, final_eta = loop_information(eta, beta_0, beta_s)
+        assert number_of_iterations == expected_iterations
+        assert final_eta == pytest.approx(expected_final_eta, rel=1e-7)
+
+    def test_within_distance(self):
+
+        center_points_within = np.array([[2, 2], [3, 2], [4, 3]])
+        center_points_not_within = np.array([[1, 1], [1, 2], [5, 5]])
+        test_point = (3, 2)
+        delta_beta = 2
+        beta_s = 3
+
+        # Test points within the threshold
+        distances_within = within_distance(center_points_within[:, 0], center_points_within[:, 1], test_point, delta_beta, beta_s)
+        assert np.all(distances_within)
+
+        # Test points not within the threshold
+        distances_not_within = within_distance(center_points_not_within[:, 0], center_points_not_within[:, 1], test_point, delta_beta, beta_s)
+        assert not np.any(distances_not_within)
+
+    def test_ABM(self):
+        """
+        Test function for ABM. It sets up the initial parameters and checks if the function returns the expected results.
+        """
+        
+        source_position = (0, 0)
+        L = 0.0004
+        beta_0 = 0.0016
+        beta_s = 1.16E-5
+        n_p = 5
+        eta = 0.7*n_p
+        number_of_iterations = 5
+        final_eta = 3.23
+        kwargs_lens = [{'theta_E': 0.0001, 'center_x': 0.000025, 'center_y': 0.00001666666}]
+
+        side_length, total_number_of_rays_shot , image_subset_centers_x, image_subset_centers_y = adaptive_boundary_mesh(source_position, L, beta_0, beta_s, n_p, eta, number_of_iterations, final_eta, kwargs_lens)
+
+        expected_number_of_subset_centers = 3576
+        expected_side_length = 6.45818E-7
+        expected_total_number_of_rays_shot= 3724
+
+        assert len(image_subset_centers_x) == len(image_subset_centers_y)
+        assert len(image_subset_centers_x) == expected_number_of_subset_centers
+        assert side_length== pytest.approx(expected_side_length, rel=1e-12)
+        assert total_number_of_rays_shot == expected_total_number_of_rays_shot
+

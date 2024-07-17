d346c65fcf23396d2f06196fd76d1235961154d9
Anowar Shajib <ajshajib@uchicago.edu>
2024-07-17 15:47:01 -0500

    Refactor Gaussian profile import in test_multi_gaussian.py

--- test/test_LensModel/test_Profiles/test_multi_gaussian.py
+++ test/test_LensModel/test_Profiles/test_multi_gaussian.py
@@ -1,7 +1,7 @@
 __author__ = "sibirrer"
 
 
-from lenstronomy.LightModel.Profiles.gaussian import Gaussian
+from lenstronomy.LightModel.Profiles.gaussian import GaussianLight
 from lenstronomy.LensModel.Profiles.gaussian import Gaussian
 from lenstronomy.LensModel.Profiles.gaussian_ellipse_potential import (
     GaussianEllipsePotential,
@@ -20,9 +20,9 @@ class TestGaussian(object):
     """Test the Gaussian lens model class."""
 
     def setup_method(self):
-        self.gaussian_kappa = MultiGaussian()
-        self.gaussian = Gaussian()
-        self.g_kappa = Gaussian()
+        self.multi_gaussian_kappa = MultiGaussian()
+        self.gaussian_light = GaussianLight()
+        self.gaussian_kappa = Gaussian()
 
     def test_derivatives(self):
         x = np.linspace(0, 5, 10)
@@ -31,7 +31,9 @@ class TestGaussian(object):
         center_x = 0.0
         center_y = 0.0
         sigma = [1.0]
-        f_x, f_y = self.gaussian_kappa.derivatives(x, y, amp, sigma, center_x, center_y)
+        f_x, f_y = self.multi_gaussian_kappa.derivatives(
+            x, y, amp, sigma, center_x, center_y
+        )
         npt.assert_almost_equal(f_x[2], 0.63813558702212059, decimal=8)
         npt.assert_almost_equal(f_y[2], 0.63813558702212059, decimal=8)
 
@@ -42,11 +44,13 @@ class TestGaussian(object):
         center_x = 0.0
         center_y = 0.0
         sigma = [1.0]
-        f_xx, f_xy, f_yx, f_yy = self.gaussian_kappa.hessian(
+        f_xx, f_xy, f_yx, f_yy = self.multi_gaussian_kappa.hessian(
             x, y, amp, sigma, center_x, center_y
         )
         kappa = 1.0 / 2 * (f_xx + f_yy)
-        kappa_true = self.gaussian.function(x, y, amp[0], sigma[0], center_x, center_y)
+        kappa_true = self.gaussian_light.function(
+            x, y, amp[0], sigma[0], center_x, center_y
+        )
         print(kappa_true)
         print(kappa)
         npt.assert_almost_equal(kappa[0], kappa_true[0], decimal=5)
@@ -60,12 +64,12 @@ class TestGaussian(object):
         center_x = 0.0
         center_y = 0.0
         sigma = [1.0]
-        f_xx, f_xy, f_yx, f_yy = self.gaussian_kappa.hessian(
+        f_xx, f_xy, f_yx, f_yy = self.multi_gaussian_kappa.hessian(
             x, y, amp, sigma, center_x, center_y
         )
         kappa = 1.0 / 2 * (f_xx + f_yy)
-        amp_3d = self.g_kappa._amp2d_to_3d(amp, sigma[0], sigma[0])
-        density_2d = self.gaussian_kappa.density_2d(
+        amp_3d = self.gaussian_kappa._amp2d_to_3d(amp, sigma[0], sigma[0])
+        density_2d = self.multi_gaussian_kappa.density_2d(
             x, y, amp_3d, sigma, center_x, center_y
         )
         npt.assert_almost_equal(kappa[1], density_2d[1], decimal=5)
@@ -75,7 +79,7 @@ class TestGaussian(object):
         amp = [1.0 * 2 * np.pi]
 
         sigma = [1.0]
-        density = self.gaussian_kappa.density(1.0, amp, sigma)
+        density = self.multi_gaussian_kappa.density(1.0, amp, sigma)
         npt.assert_almost_equal(density, 0.6065306597126334, decimal=8)
 
 

c7bfae17aebc1c066d0a29852b81bfc0ff4df3af
Anowar Shajib <ajshajib@uchicago.edu>
2024-09-10 21:04:50 -0500

    Refactor GNFW profile tests for consistency and accuracy

--- test/test_LensModel/test_Profiles/test_gnfw.py
+++ test/test_LensModel/test_Profiles/test_gnfw.py
@@ -45,6 +45,14 @@ class TestGNFW(object):
         values_gnfw = self.gnfw.function(x, y, Rs, alpha_Rs, gamma_in)
         npt.assert_almost_equal(values_nfw, values_gnfw, decimal=4)
 
+        # test for really small Rs
+        Rs = 0.00000001
+        alpha_Rs_nfw = self.nfw.rho02alpha(rho0, Rs)
+        alpha_Rs = self.gnfw.rho02alpha(rho0, Rs, gamma_in)
+        values_nfw = self.nfw.function(x, y, Rs, alpha_Rs_nfw)
+        values_gnfw = self.gnfw.function(x, y, Rs, alpha_Rs, gamma_in)
+        npt.assert_almost_equal(values_nfw, values_gnfw, decimal=10)
+
     def test_derivatives(self):
         """Tests `GNFW.derivatives()`"""
         x = np.array([1.5])
@@ -59,8 +67,8 @@ class TestGNFW(object):
         alpha_Rs = self.gnfw.rho02alpha(rho0, Rs, gamma_in)
         f_x_gnfw, f_y_gnfw = self.gnfw.derivatives(x, y, Rs, alpha_Rs, gamma_in)
 
-        npt.assert_almost_equal(f_x_nfw, f_x_gnfw, decimal=10)
-        npt.assert_almost_equal(f_y_nfw, f_y_gnfw, decimal=10)
+        npt.assert_almost_equal(f_x_nfw, f_x_gnfw, decimal=8)
+        npt.assert_almost_equal(f_y_nfw, f_y_gnfw, decimal=8)
 
         f_x_gnfwt, f_y_gnfwt = self.gnfw_trapezoidal.derivatives(
             x, y, Rs, alpha_Rs, gamma_in
@@ -70,10 +78,12 @@ class TestGNFW(object):
 
         # test for really small Rs
         Rs = 0.00000001
+        alpha_Rs_nfw = self.nfw.rho02alpha(rho0, Rs)
+        alpha_Rs = self.gnfw.rho02alpha(rho0, Rs, gamma_in)
         f_x_nfw, f_y_nfw = self.nfw.derivatives(x, y, Rs, alpha_Rs_nfw)
         f_x_gnfw, f_y_gnfw = self.gnfw.derivatives(x, y, Rs, alpha_Rs, gamma_in)
-        npt.assert_almost_equal(f_x_nfw, f_x_gnfw, decimal=3)
-        npt.assert_almost_equal(f_y_nfw, f_y_gnfw, decimal=3)
+        npt.assert_almost_equal(f_x_nfw, f_x_gnfw, decimal=5)
+        npt.assert_almost_equal(f_y_nfw, f_y_gnfw, decimal=5)
 
     def test_hessian(self):
         """Tests `GNFW.hessian()`"""
@@ -91,9 +101,9 @@ class TestGNFW(object):
             x, y, Rs, alpha_Rs, gamma_in
         )
 
-        npt.assert_almost_equal(f_xx_nfw, f_xx_gnfw, decimal=10)
-        npt.assert_almost_equal(f_yy_nfw, f_yy_gnfw, decimal=10)
-        npt.assert_almost_equal(f_xy_nfw, f_xy_gnfw, decimal=10)
+        npt.assert_almost_equal(f_xx_nfw, f_xx_gnfw, decimal=8)
+        npt.assert_almost_equal(f_yy_nfw, f_yy_gnfw, decimal=8)
+        npt.assert_almost_equal(f_xy_nfw, f_xy_gnfw, decimal=8)
 
         f_xx_gnfwt, f_xy_gnfwt, _, f_yy_gnfwt = self.gnfw_trapezoidal.hessian(
             x, y, Rs, alpha_Rs, gamma_in
@@ -104,6 +114,8 @@ class TestGNFW(object):
 
         # test for really small Rs
         Rs = 0.00000001
+        alpha_Rs_nfw = self.nfw.rho02alpha(rho0, Rs)
+        alpha_Rs = self.gnfw.rho02alpha(rho0, Rs, gamma_in)
         f_xx_nfw, f_xy_nfw, _, f_yy_nfw = self.nfw.hessian(x, y, Rs, alpha_Rs_nfw)
         f_xx_gnfw, f_xy_gnfw, _, f_yy_gnfw = self.gnfw.hessian(
             x, y, Rs, alpha_Rs, gamma_in
@@ -141,7 +153,7 @@ class TestGNFW(object):
         density_gnfw = self.gnfw.density_lens(R, Rs, alpha_Rs, gamma_in)
         density_gnfwt = self.gnfw_trapezoidal.density_lens(R, Rs, alpha_Rs, gamma_in)
 
-        npt.assert_almost_equal(density_nfw, density_gnfw, decimal=10)
+        npt.assert_almost_equal(density_nfw, density_gnfw, decimal=9)
         npt.assert_almost_equal(density_nfw, density_gnfwt, decimal=6)
 
     def test_density_2d(self):
@@ -183,7 +195,7 @@ class TestGNFW(object):
         alpha_Rs = self.gnfw.rho02alpha(rho0, Rs, gamma_in)
         mass_3d_gnfw = self.gnfw.mass_3d_lens(r, Rs, alpha_Rs, gamma_in)
 
-        npt.assert_almost_equal(mass_3d_nfw, mass_3d_gnfw, decimal=10)
+        npt.assert_almost_equal(mass_3d_nfw, mass_3d_gnfw, decimal=8)
 
 
 if __name__ == "__main__":

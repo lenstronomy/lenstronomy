885f0323d3c360bf43f82c31d51a7b7e6393b3f6
Daniel Gilman <gilmanda@ucla.edu>
2024-11-13 00:24:40 -0600

    add profile class

--- lenstronomy/LensModel/Solver/solver4point.py
+++ lenstronomy/LensModel/Solver/solver4point.py
@@ -28,7 +28,7 @@ class Solver4Point(object):
             "EPL_BOXYDISKY",
             "EPL_BOXYDISKY_ELL",
             "EPL_MULTIPOLE_M3M4",
-            "EPL_MULTIPOLE_M1M3M4"
+            "EPL_MULTIPOLE_M1M3M4",
             "EPL_MULTIPOLE_M3M4_ELL",
             "EPL_MULTIPOLE_M1M3M4_ELL"
         ]:

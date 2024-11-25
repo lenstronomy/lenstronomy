a1e63a3b7de32e30eb756652a7b3f768c47826c7
Simon Birrer <sibirrer@users.noreply.github.com>
2024-11-25 13:17:04 -0500

    new definition for changing Einstein radius of a power-law profile as a function of source redshift and kappa_ext

--- lenstronomy/Cosmo/lens_cosmo.py
+++ lenstronomy/Cosmo/lens_cosmo.py
@@ -501,3 +501,24 @@ class LensCosmo(object):
         return self.background.beta_double_source_plane(
             z_lens=z_lens, z_source_1=z_source_1, z_source_2=z_source_2
         )
+
+    def theta_E_power_law_scaling(self, theta_E_convention, kappa_ext_convention, gamma_pl,
+                                  z_lens, z_source_convention, z_source):
+
+        if z_source == z_source_convention:
+            theta_E = theta_E_convention
+            kappa_ext = kappa_ext_convention
+        else:
+            beta = self.beta_double_source_plane(
+                z_lens=z_lens,
+                z_source_2=z_source_convention,
+                z_source_1=z_source,
+            )
+            #theta_E = theta_E_convention * beta ** (1.0 / (gamma_pl - 1))
+            kappa_ext = kappa_ext_convention * beta
+            theta_E = theta_E_convention * beta ** (1. / (gamma_pl - 1.))
+            # theta_E = theta_E_convention * (beta + kappa_ext_convention * (1 - beta)) ** (1. / (gamma_pl - 1.))
+            # β − (1 − λ)(1 − β) ** (1.0 / (gamma_pl - 1))
+
+        theta_E /= (1 - kappa_ext) ** (1.0 / (gamma_pl - 1))
+        return theta_E

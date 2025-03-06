73bc17018fd39c2e1d5b8d8313992ab5c37e3bb2
ahuang314 <alan.huang.1@stonybrook.edu>
2025-03-06 11:40:23 -0500

    comment change

--- lenstronomy/PointSource/Types/base_ps.py
+++ lenstronomy/PointSource/Types/base_ps.py
@@ -25,18 +25,14 @@ class PSBase(object):
         :param index_lens_model_list: list (length of different patches/bands) of integer lists, e.g., [[0, 1], [2, 3]];
             evaluating a subset of the lens models per individual bands. If this keyword is set, the image positions need
             to have a specified band/frame assigned to it
-        :param point_source_frame_list: list of ints, e.g. we can assign the i-th image to the second band
-            with point_source_frame_list[i] = 2
+        :param point_source_frame_list: list of ints mirroring the structure of the image positions.
+            Integers correspond to the i'th list entry of index_lens_model_list indicating in which frame/band the image is
+            appearing
         :param redshift: redshift of the source, only required for multiple source redshifts
         :type redshift: None or float
         """
         self._redshift = redshift
         self._lens_model = lens_model
-
-        # k_list is a list of lists which combines index_lens_model_list with point_source_frame_list
-        # to connect each point source image with its corresponding lens models
-        # e.g. k_list[i] = [a, b] means that the i-th point source image only involves the models
-        # lens_model_list[a] and lens_model_list[b]
         if index_lens_model_list is not None:
             k_list = []
             for point_source_frame in point_source_frame_list:

681fbd66c3075d6d2e8d58506192aa589e75920e
Simon Birrer <sibirrer@users.noreply.github.com>
2025-05-27 21:33:49 -0400

    ready for release 1.13.1

--- lenstronomy/__init__.py
+++ lenstronomy/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "lenstronomy developers"
 __email__ = "lenstronomy-dev@googlegroups.com"
-__version__ = "1.13.0"
+__version__ = "1.13.1"
 __credits__ = "lenstronomy developers"
 
 from .Util.package_util import short, laconic

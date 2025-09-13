f6c1dabf300e9387dbb08816f12908fbdfd975ac
Nanz <zn95330@outlook.com>
2025-09-12 19:27:42 -0500

    Add comments

--- lenstronomy/ImSim/image_model.py
+++ lenstronomy/ImSim/image_model.py
@@ -214,7 +214,8 @@ class ImageModel(object):
         :param unconvolved: if True: returns the unconvolved light distribution (prefect
             seeing)
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :param de_lensed: if True: returns the un-lensed source surface brightness
             profile, otherwise the lensed.
         :param k: integer, if set, will only return the model of the specific index
@@ -267,7 +268,8 @@ class ImageModel(object):
         :param unconvolved: if True: returns the unconvolved light distribution (prefect
             seeing)
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :param de_lensed: if True: returns the un-lensed source surface brightness
             profile, otherwise the lensed.
         :param k: integer, if set, will only return the model of the specific index
@@ -307,7 +309,8 @@ class ImageModel(object):
         :param kwargs_extinction: list of keyword arguments of extinction model
         :param kwargs_special: list of special keyword arguments
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :param de_lensed: if True: returns the un-lensed source surface brightness
             profile, otherwise the lensed.
         :param k: integer, if set, will only return the model of the specific index
@@ -402,7 +405,8 @@ class ImageModel(object):
         :param unconvolved: if True, returns unconvolved surface brightness (perfect
             seeing), otherwise convolved with PSF kernel
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :return: 2d array of surface brightness pixels
         """
         if self._pixelbased_bool is True:
@@ -427,7 +431,8 @@ class ImageModel(object):
         :param unconvolved: if True, returns unconvolved surface brightness (perfect
             seeing), otherwise convolved with PSF kernel
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :return: 2d array of surface brightness pixels
         """
         ra_grid, dec_grid = self.ImageNumerics.coordinates_evaluate
@@ -525,7 +530,8 @@ class ImageModel(object):
         :param unconvolved: if True: returns the unconvolved light distribution (prefect
             seeing)
         :param apply_primary_beam: if True: returns the light distribution affected by the 
-            interferometry primary beam
+            interferometry primary beam. This only applies when the class instance has a primary beam
+            (for interferometric image fittings).
         :param source_add: if True, compute source, otherwise without
         :param lens_light_add: if True, compute lens light, otherwise without
         :param point_source_add: if True, add point sources, otherwise without

b185d3ff7c72189eea959cf0d99ca93c2e79e653
Simon Birrer <sibirrer@users.noreply.github.com>
2026-01-30 15:30:19 -0500

    make it such that the option of source_position_likelihood=False can be chosen for LENSED_POSITION (just not as default)

--- lenstronomy/Sampling/Likelihoods/position_likelihood.py
+++ lenstronomy/Sampling/Likelihoods/position_likelihood.py
@@ -18,7 +18,7 @@ class PositionLikelihood(object):
         image_position_likelihood=False,
         ra_image_list=None,
         dec_image_list=None,
-        source_position_likelihood=False,
+        source_position_likelihood=None,
         source_position_tolerance=None,
         source_position_sigma=0.001,
         force_no_add_image=False,
@@ -56,8 +56,12 @@ class PositionLikelihood(object):
         # TODO replace with public function of ray_shooting
         self._lensModel = point_source_class._lens_model
         point_source_type_list = point_source_class.point_source_type_list
-        if "LENSED_POSITION" in point_source_type_list:
-            source_position_likelihood = True
+
+        if source_position_likelihood is None:
+            if "LENSED_POSITION" in point_source_type_list:
+                source_position_likelihood = True
+            else:
+                source_position_likelihood = False
         self._astrometric_likelihood = astrometric_likelihood
         self._image_position_sigma = image_position_uncertainty
         self._source_position_sigma = source_position_sigma

57f06512c72d7b6cc7a2da9202f28bd4bd122217
Anowar Shajib <ajshajib@gmail.com>
2026-05-05 22:13:41 -0500

    Refactor docstring formatting in NautilusSampler
    Removed unnecessary line breaks in the docstring.

--- lenstronomy/Sampling/Samplers/nautilus_sampler.py
+++ lenstronomy/Sampling/Samplers/nautilus_sampler.py
@@ -13,9 +13,7 @@ __all__ = ["NautilusSampler"]
 
 
 class NautilusSampler(NestedSampler):
-    """Wrapper for the nautilus sampler by Johannes U.
-
-    Lange.
+    """Wrapper for the nautilus sampler by Johannes U. Lange.
         paper : https://arxiv.org/abs/2306.16923
         doc : https://nautilus-sampler.readthedocs.io
     """

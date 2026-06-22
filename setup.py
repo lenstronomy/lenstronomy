6c478a6667dd7474447028a5d48e0831f00441d8
ahuang314 <alan.huang.1@stonybrook.edu>
2026-06-22 13:45:54 -0400

    try python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -85,8 +85,9 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     tests_require=tests_require,
 )
